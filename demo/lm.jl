d13930837816505d25454e016da0eea4566940f8
John Myles White <jmw@johnmyleswhite.com>
2012-07-12 22:37:02 -0700

    Correct std. errors, t-values and p-values

--- demo/lm.jl
+++ demo/lm.jl
@@ -1,3 +1,5 @@
+load(path_expand("~/julia/base/distributions.jl"))
+
 type OLSResults
   call::Formula
   predictors::Matrix
@@ -13,25 +15,47 @@ type OLSResults
 end
 
 function print(results::OLSResults)
+  function p_value_stars(p_value::Float64)
+    if 0.05 < p_value < 0.1
+      return "."
+    elseif 0.01 < p_value < 0.05
+      return "*"
+    elseif 0.001 < p_value < 0.01
+      return "**"
+    elseif p_value < 0.001
+      return "***"
+    else
+      return " "
+    end
+  end  
   println()
   println()
-  println("Call to lm(): $(results.call)")
+  println("Call:")
+  println("lm($(results.call))") # Should print df as well.
   println()
-  println("Fitted Model:")
+  println("Coefficients:")
   println()
-  printf(" %16.s  %8.s  %8.s  %8.s  %8.s\n", "Term", "Estimate", "Std. Error", "t", "p-Value")
+  printf(" %16.s %12s %12s %12s %12s\n", "Term", "Estimate", "Std. Error", "t value", "Pr(>|t|)")
   N = size(results.coefficients, 1)
   for i = 1:N
     printf(" %16.s  ", results.predictor_names[i])
-    println(join(map(z -> sprintf("%5.7f", z), {results.coefficients[i, 1],
-                  results.std_errors[i, 1],
-                  results.t_stats[i, 1],
-                  results.p_values[i, 1]}), "  "))
+    print(join(map(z -> sprintf("%12.5f", z),
+                    {results.coefficients[i, 1],
+                     results.std_errors[i, 1],
+                     results.t_stats[i, 1],
+                     results.p_values[i, 1]}), " "))
+    print(" ")
+    println(p_value_stars(results.p_values[i, 1]))
   end
+  println("---")
+  println("Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1")
+  println()  
+  printf("Adjusted R-squared: %0.4f", results.r_squared)
   println()
-  printf("R-squared: %0.4f", results.r_squared)
   println()
-  println()
+  #Residual standard error: 0.04082 on 2 degrees of freedom
+  #Multiple R-squared: 0.9994,	Adjusted R-squared: 0.9985 
+  #F-statistic:  1081 on 3 and 2 DF,  p-value: 0.0009244 
 end
 
 # minimal first version: support y ~ x1 + x2 + log(x3)
@@ -48,8 +72,9 @@ function lm(ex::Expr, df::DataFrame)
   residuals = y - predictions
   degrees = n - p
   sigma = sum(residuals.^2) / degrees
-  covariance = sigma * inv(x' * x)
-  t_values = coefficients ./ (diag(covariance) / sqrt(n))
+  covariance = (sigma * inv(x' * x)).^(1/2)
+  t_values = coefficients ./ diag(covariance)
+  p_values = map(t -> 2 * (1 - cdf(TDist(degrees), t)), t_values)
   r_squared = 1.0 - sigma / var(y)
   OLSResults(call,
              x,
@@ -58,7 +83,7 @@ function lm(ex::Expr, df::DataFrame)
              coefficients,
              diag(covariance),
              t_values,
-             t_values,
+             p_values,
              predictions,
              residuals,
              r_squared)
