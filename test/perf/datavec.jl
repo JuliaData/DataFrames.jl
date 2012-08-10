eb6f1a8a95276a16e67d53eef3c25655b636c715
Tom Short <t.short@ieee.org>
2012-08-10 08:10:17 -0400

    A few more perf tests.

--- test/perf/datavec.jl
+++ test/perf/datavec.jl
@@ -4,6 +4,8 @@ v = randn(N)
 dv = DataVec(v)
 dvna = copy(dv)
 dvna[randi(N, 10000)] = NA
+idxv = shuffle([1:N])
+idxdv = DataVec(idxv)
 
 f1(v) = sum(v)
 f2(v) = sum(nafilter(v))
@@ -11,6 +13,9 @@ f3(v) = sum(naFilter(v))
 f4(v) = mean(v)
 f5(v) = mean(nafilter(v))
 f6(v) = mean(naFilter(v))
+f7(v1,v2) = v1 + v2
+f8(v1,v2) = v1 .> v2
+f9(v,i) = v[i]
 
 perf_test = Dict()
 
@@ -32,7 +37,20 @@ perf_test["mean(dvna): DataVec with NA's"] = () -> f4(dv)
 perf_test["mean(nafilter(dvna)): DataVec with NA's"] = () -> f5(dv)
 perf_test["mean(naFilter(dvna)): DataVec with NA's"] = () -> f6(dv)
 
+perf_test["v + 1.0 : Vector"] = () -> f7(v,1.0)
+perf_test["dv + 1.0 : DataVec with no NA's"] = () -> f7(dv,1.0)
+perf_test["dvna + 1.0 : DataVec with NA's"] = () -> f7(dvna,1.0)
 
+perf_test["v .> 1.0 : Vector"] = () -> f8(v,1.0)
+perf_test["dv .> 1.0 : DataVec with no NA's"] = () -> f8(dv,1.0)
+perf_test["dvna .> 1.0 : DataVec with NA's"] = () -> f8(dvna,1.0)
+
+perf_test["v[idxv] : Vector"] = () -> f9(v,idxv)
+perf_test["dv[idxv] : DataVec and Vector indexing"] = () -> f9(dv,idxv)
+perf_test["dv[idxdv] : DataVec and DataVec indexing"] = () -> f9(dv,idxdv)
+
+
+tm = strftime("%Y-%m-%d %H:%M:%S",int(time()))
 for (name, f) in perf_test
     for i in 1:3     # do each three times
         res = try
@@ -40,7 +58,7 @@ for (name, f) in perf_test
         catch
             NA
         end
-        println(name, ", ", res, ", ", strftime("%Y-%m-%d %H:%M:%S",int(time())))
+        println(name, ", ", res, ", ", tm) 
     end
 end
 
