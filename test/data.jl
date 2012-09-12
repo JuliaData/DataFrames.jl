67bb87f3ab9eeabfd3ba433dfb347c231c90e1c3
Chris DuBois <chris.dubois@gmail.com>
2012-09-12 13:37:41 -0700

    Add tests for PooledDataVec helper functions.

--- test/data.jl
+++ test/data.jl
@@ -37,6 +37,9 @@ pdvpp = PooledDataVec([1,2,2,3,2,1], [1,2,3,4])
 @test pdvpp.pool == [1,2,3,4]
 @test string(pdvpp) == "[1,2,2,3,2,1]"
 pdvpp = PooledDataVec(["one","two","two"], ["one","two","three"])
+@test all(values(pdvpp) .== ["one","two","two"])
+@test all(indices(pdvpp) .== uint32([1,3,3]))
+@test all(levels(pdvpp) .== ["one","three","two"])
 @test pdvpp.pool == ["one","three","two"]
 @test string(pdvpp) == "[one,two,two]"
 @test throws_exception(PooledDataVec(["one","two","four"], ["one","two","three"]), Exception)
