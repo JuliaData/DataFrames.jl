0b5b8247ec5247952f696718f60d2164ef0f42c0
John Myles White <jmw@johnmyleswhite.com>
2012-12-07 16:26:13 -0500

    Revised example of failure induced by load()

--- src/tmp.jl
+++ src/tmp.jl
@@ -1,11 +1,12 @@
 load("DataFrames")
 using DataFrames
 
-# fn = "/Users/johnmyleswhite/julia/extras/bitarray.jl"
-# include(fn)
+dv = DataVec([1, 2, 3], [false, false, false])
 
-# DataVec([1, 2, 3], bitpack([false, false, false]))
+fn = "/Users/johnmyleswhite/julia/extras/bitarray.jl"
+include(fn)
 
+DataVec([1, 2, 3], bitpack([false, false, false]))
 dv = DataVec([1, 2, 3], [false, false, false])
 
 failNA(dv)
