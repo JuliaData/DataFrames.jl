89f1303f0ddc4eed70ad13d3846a4ad6bcba056a
Tom Short <t.short@ieee.org>
2013-04-08 19:05:17 -0400

    Add "each" capability to `rep`.

--- src/extras.jl
+++ src/extras.jl
@@ -141,10 +141,28 @@ function rep(x::AbstractVector, lengths::Vector{Int})
     end
     res
 end
+rep(x::AbstractVector, times::AbstractVector{Int}, each::Int) = rep(x, times)
 
-function rep(x::Any, length::Int)
-    [fill(x, length)...]
+function rep(x::AbstractVector, times::Int = 1, each::Int = 1)
+    res = similar(x, each * sum(times) * length(x))
+    i = 1
+    for jdx in 1:times
+        for idx in 1:length(x)
+            for kdx in 1:each
+                res[i] = x[idx]
+                i += 1
+            end
+        end
+    end
+    res
 end
+
+rep(x, times) = fill(x, times)
+
+function rep(x; times = 1, each::Int = 1)
+    rep(x, times, each)
+end
+
 
 ##############################################################################
 ##
