15d4c83fb86394ba7c3faa6f75b14460d5e52f30
Rene Donner <code@donner.at>
2015-05-11 17:03:57 +0200

    separate method for VectorNone

--- src/abstractdataframe/abstractdataframe.jl
+++ src/abstractdataframe/abstractdataframe.jl
@@ -656,8 +656,8 @@ Base.vcat(df::AbstractDataFrame) = df
 
 Base.vcat(dfs::AbstractDataFrame...) = vcat(collect(dfs))
 
+Base.vcat(dfs::Vector{None}) = dfs
 function Base.vcat{T<:AbstractDataFrame}(dfs::Vector{T})
-    isempty(dfs) && return dfs
     coltyps, colnams, similars = _colinfo(dfs)
 
     res = DataFrame()
