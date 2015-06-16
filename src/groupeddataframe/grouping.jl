bc54c4ba97a7b8e285aa2d454b7dd33c66552380
matthieugomez <Matthieu@dynamic-oit-fishnet-d-25.Princeton.EDU>
2015-06-16 13:09:35 -0400

    don’t use dropunused

--- src/groupeddataframe/grouping.jl
+++ src/groupeddataframe/grouping.jl
@@ -136,7 +136,8 @@ _names(gd::GroupedDataFrame) = _names(gd.parent)
 
 # group creates pooled data array from multiple columns 
 function group(df::AbstractDataFrame) 
-	ncols = length(df)
+    # from groupby
+    ncols = length(df)
     dv = DataArrays.PooledDataArray(df[ncols])
     dv_has_nas = (findfirst(dv.refs, 0) > 0 ? 1 : 0)
     x = copy(dv.refs) .+ dv_has_nas
@@ -149,7 +150,11 @@ function group(df::AbstractDataFrame)
         end
         ngroups = ngroups * (length(dv.pool) + dv_has_nas)
     end
-    dropUnusedLevels!(x)
+    # factorize
+    uu = unique(x)
+    T = eltype(x)
+    dict = Dict(uu, map(z -> convert(T,z), 1:length(uu)))
+    PooledDataArray(DataArrays.RefArray(map(z -> dict[z], x)),  [1:length(uu)])
 end
 ##############################################################################
 ##
