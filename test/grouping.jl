c2234f15dc7b784d5afa7054ad4b32cf46c744e6
Matthieu Gomez <gomez.matthieu@gmail.com>
2015-06-17 19:00:21 -0400

    Update grouping.jl
    Change name in test + add refs type test

--- test/grouping.jl
+++ test/grouping.jl
@@ -33,10 +33,15 @@ module TestGrouping
 
     df = DataFrame(v1 = @data([1, NA, 2, 1]), v2 = @data([1, 3, 2, 1]), v3 = @data([NA, 1, 1, NA]))
     
-    @test poolall(df, skipna = false).pool == [1, 2, 3]
-    @test poolall(df, skipna = false).refs == [1, 2, 3, 1]
-    @test poolall(df).pool == [1]
-    @test poolall(df).refs = [0, 0, 1, 0]
+    @test group(df, skipna = false).pool == [1, 2, 3]
+    @test group(df, skipna = false).refs == [1, 2, 3, 1]
+    @test group(df).pool == [1]
+    @test group(df).refs = [0, 0, 1, 0]
+    
+    # test that type of refs of last column is promoted
+    df = DataFrame(v1 = pool(1:1000), v2 = pool(fill(1, 1000)))
+    @test groupby(df, [:v1, :v2]).starts  [1:100;]
+    
 
 
 end
