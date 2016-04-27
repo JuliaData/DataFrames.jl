3df4b0f8e81156dc286d930d30857cc8e5b79889
autodocs <autodocs>
2016-04-27 17:31:35 +0000

    build based on 9a95c50

--- latest/assets/mathjaxhelper.js
+++ latest/assets/mathjaxhelper.js
@@ -1,8 +1,25 @@
 MathJax.Hub.Config({
-  "tex2jax": { inlineMath: [ [ '$', '$' ] ] }
+  "tex2jax": {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes: true
+  }
 });
 MathJax.Hub.Config({
   config: ["MMLorHTML.js"],
-  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
-  extensions: ["MathMenu.js", "MathZoom.js"]
+  jax: [
+    "input/TeX",
+    "output/HTML-CSS",
+    "output/NativeMML"
+  ],
+  extensions: [
+    "MathMenu.js",
+    "MathZoom.js",
+    "TeX/AMSmath.js",
+    "TeX/AMSsymbols.js",
+    "TeX/autobold.js",
+    "TeX/autoload-all.js"
+  ]
+});
+MathJax.Hub.Config({
+  TeX: { equationNumbers: { autoNumber: "AMS" } }
 });
