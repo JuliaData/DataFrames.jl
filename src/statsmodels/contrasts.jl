faa592f86aaa8f80870db1548d825de17bf40fd9
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-05-11 09:28:29 -0400

    code review
    * ## -> #
    * more comments
    * clean up extra space
    * type parameters
    * more informative error messages

--- src/statsmodels/contrasts.jl
+++ src/statsmodels/contrasts.jl
@@ -1,40 +1,38 @@
-## Specify contrasts for coding categorical data in model matrix. Contrasts types
-## are a subtype of AbstractContrasts. ContrastsMatrix types hold a contrast
-## matrix, levels, and term names and provide the interface for creating model
-## matrix columns and coefficient names.
-##
-## Contrasts types themselves can be instantiated to provide containers for
-## contrast settings (currently, just the base level).
-##
-## ModelFrame will hold a Dict{Symbol, ContrastsMatrix} that maps column
-## names to contrasts.
-##
-## ModelMatrix will check this dict when evaluating terms, falling back to a
-## default for any categorical data without a specified contrast.
-##
-## TODO: implement contrast types in Formula/Terms
-##
-## Dave Kleinschmidt 2015-2016
+# Specify contrasts for coding categorical data in model matrix. Contrasts types
+# are a subtype of AbstractContrasts. ContrastsMatrix types hold a contrast
+# matrix, levels, and term names and provide the interface for creating model
+# matrix columns and coefficient names.
+#
+# Contrasts types themselves can be instantiated to provide containers for
+# contrast settings (currently, just the base level).
+#
+# ModelFrame will hold a Dict{Symbol, ContrastsMatrix} that maps column
+# names to contrasts.
+#
+# ModelMatrix will check this dict when evaluating terms, falling back to a
+# default for any categorical data without a specified contrast.
+#
+# TODO: implement contrast types in Formula/Terms
 
 
 """
 Interface to describe contrast coding schemes for categorical variables.
 
 Concrete subtypes of `AbstractContrasts` describe a particular way of converting a
-categorical column in a `DataFrame` into numeric columns in a `ModelMatrix`. Each
+categorical data vector into numeric columns in a `ModelMatrix`. Each
 instantiation optionally includes the levels to generate columns for and the base
 level. If not specified these will be taken from the data when a `ContrastsMatrix` is
 generated (during `ModelFrame` construction).
 
-## Constructors
+# Constructors
 
 For `C <: AbstractContrast`:
 
 ```julia
-C()                             # levels are inferred later 
-C(levels = ::Vector{Any})       # levels checked against data later
-C(base = ::Any)                 # specify base level
-C(levels = ::Vector{Any}, base = ::Any)
+C()                                     # levels are inferred later 
+C(levels = ::Vector{Any})               # levels checked against data later
+C(base = ::Any)                         # specify base level
+C(levels = ::Vector{Any}, base = ::Any) # specify levels and base
 ```
 
 If specified, levels will be checked against data when generating a
@@ -50,15 +48,15 @@ level.
 Both `levels` and `base` will be coerced to the type of the data when
 constructing a `ContrastsMatrix`.
 
-## Concrete types
+# Concrete types
 
 * `TreatmentContrasts`
 * `SumContrasts`
 * `HelmertContrasts`
 
-To implement your own concrete types, implement a constructor and a method for
-constructing the actual contrasts matrix that maps from levels to `ModelMatrix`
-column values:
+To implement your own concrete types, implement a constructor and a
+`contrast_matrix` method for constructing the actual contrasts matrix
+that maps from levels to `ModelMatrix` column values:
 
 ```julia
 type MyContrasts <: AbstractContrasts
@@ -71,49 +69,50 @@ contrasts_matrix(C::MyContrasts, baseind, n) = ...
 """
 abstract AbstractContrasts
 
-## Contrasts + Data = ContrastsMatrix
-type ContrastsMatrix{T}
+# Contrasts + Levels (usually from data) = ContrastsMatrix
+type ContrastsMatrix{T, S <: AbstractContrasts}
     matrix::Matrix{Float64}
     termnames::Vector{T}
     levels::Vector{T}
-    contrasts::AbstractContrasts
+    contrasts::S
 end
 
 """
-    ContrastsMatrix{T}(::AbstractContrasts, levels::Vector{T})
+    ContrastsMatrix{T}(C::AbstractContrasts, levels::Vector{T})
 
-Instantiate contrasts matrix for given data (categorical levels)
+Compute contrasts matrix for a given set of categorical data levels.
 
 If levels are specified in the `AbstractContrasts`, those will be used, and likewise
 for the base level (which defaults to the first level).
 """
-function ContrastsMatrix{T}(C::AbstractContrasts, lvls::Vector{T})
+function ContrastsMatrix{T}(C::AbstractContrasts, levels::Vector{T})
 
-    ## if levels are defined on C, use those, validating that they line up.
-    ## what does that mean?
-    ##
-    ## C.levels == lvls (best case)
-    ## data levels missing from contrast: would generate empty/undefined rows. 
-    ## better to filter data frame first
-    ## contrast levels missing from data: would have empty columns, generate a
-    ## rank-deficient model matrix.
-    c_lvls = convert(typeof(lvls), get(C.levels, lvls))
-    mismatched_lvls = symdiff(c_lvls, lvls)
-    isempty(mismatched_lvls) || error("Contrasts levels not found in data or vice-versa: ", mismatched_lvls)
+    # if levels are defined on C, use those, validating that they line up.
+    # what does that mean? either:
+    #
+    # 1. C.levels == levels (best case)
+    # 2. data levels missing from contrast: would generate empty/undefined rows. 
+    #    better to filter data frame first
+    # 3. contrast levels missing from data: would have empty columns, generate a
+    #    rank-deficient model matrix.
+    c_levels = oftype(levels, get(C.levels, levels))
+    mismatched_levels = symdiff(c_levels, levels)
+    isempty(mismatched_levels) || error("contrasts levels not found in data or vice-versa: $mismatched_levels.\nData levels: $levels.\nContrast levels: $c_levels")
 
-    n = length(c_lvls)
-    n > 1 || error("not enough degrees of freedom to define contrasts")
-    
-    ## find index of base level. use C.base, then default (1).
-    baseind = isnull(C.base) ? 1 : findfirst(c_lvls, convert(eltype(lvls), get(C.base)))
-    baseind > 0 || error("Base level $(C.base) not found in levels")
+    n = length(c_levels)
+    n == 0 && error("empty set of levels found (need at least two to compute contrasts).")
+    n == 1 && error("only one level found: $(c_levels[1]). need at least two to compute contrasts.")
     
     not_base = [1:(baseind-1); (baseind+1):n]
     tnames = c_lvls[not_base]
+    # find index of base level. use C.base, then default (1).
+    baseind = isnull(C.base) ? 1 : findfirst(c_levels, convert(eltype(levels), get(C.base)))
+    baseind > 0 || error("base level $(C.base) not found in levels $c_levels.")
+
 
     mat = contrasts_matrix(C, baseind, n)
 
-    ContrastsMatrix(mat, tnames, c_lvls, C)
+    ContrastsMatrix(mat, tnames, c_levels, C)
 end
 
 ContrastsMatrix(C::AbstractContrasts, v::PooledDataArray) = ContrastsMatrix(C, levels(v))
@@ -122,9 +121,8 @@ ContrastsMatrix(C::AbstractContrasts, v::PooledDataArray) = ContrastsMatrix(C, l
 nullify(x::Nullable) = x
 nullify(x) = Nullable(x)
 
-## Making a contrast type T only requires that there be a method for
-## contrasts_matrix(T, v::PooledDataArray). The rest is boilerplate.
-##
+# Making a contrast type T only requires that there be a method for
+# contrasts_matrix(T, v::PooledDataArray). The rest is boilerplate.
 for contrastType in [:TreatmentContrasts, :SumContrasts, :HelmertContrasts]
     @eval begin
         type $contrastType <: AbstractContrasts
@@ -146,19 +144,19 @@ end
 One indicator (1 or 0) column for each level, __including__ the base level.
 
 Needed internally when a term is non-redundant with lower-order terms (e.g., in
-~0+x vs. ~1+x, or in the interactions terms in ~1+x+x&y vs. ~1+x+y+x&y. In the
-non-redundant cases, we can (probably) expand x into length(levels(x)) columns
+`~0+x` vs. `~1+x`, or in the interactions terms in `~1+x+x&y` vs. `~1+x+y+x&y`. In the
+non-redundant cases, we can expand x into `length(levels(x))` columns
 without creating a non-identifiable model matrix (unless the user has done
-something dumb in specifying the model, which we can't do much about anyway).
+something foolish in specifying the model, which we can't do much about anyway).
 """
 type DummyContrasts <: AbstractContrasts
-## Dummy contrasts have no base level (since all levels produce a column)
+# Dummy contrasts have no base level (since all levels produce a column)
 end
 
 ContrastsMatrix{T}(C::DummyContrasts, lvls::Vector{T}) = ContrastsMatrix(eye(Float64, length(lvls)), lvls, lvls, C)
 
-## Default for promoting contrasts to full rank is to convert to dummy contrasts
-## promote_contrast(C::AbstractContrasts) = DummyContrasts(eye(Float64, length(C.levels)), C.levels, C.levels)
+# Default for promoting contrasts to full rank is to convert to dummy contrasts
+# promote_contrast(C::AbstractContrasts) = DummyContrasts(eye(Float64, length(C.levels)), C.levels, C.levels)
 
 "Promote a contrast to full rank"
 promote_contrast(C::ContrastsMatrix) = ContrastsMatrix(DummyContrasts(), C.levels)
@@ -226,7 +224,7 @@ function contrasts_matrix(C::HelmertContrasts, baseind, n)
         mat[i+1, i] = i
     end
 
-    ## re-shuffle the rows such that base is the all -1.0 row (currently first)
+    # re-shuffle the rows such that base is the all -1.0 row (currently first)
     mat = mat[[baseind; 1:(baseind-1); (baseind+1):end], :]
     return mat
 end
