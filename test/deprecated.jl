d1fc34062eae0b2a899e44e596e20105d900a336
Tony Kelman <tony@kelman.net>
2016-09-06 14:25:00 -0700

    🚮 delete temp files created during testing (#1062)
    * 🚮 delete temp file when test/deprecated.jl is done
    
    * fix usage of mktemp
    
    it returns `(filename, io)`, though the `io` is read-only by default so still need the
    `open(fn, "w")`
    
    * litter cleanup in test/io.jl too
    
    `rm(tf)` before overwriting it with a new `tf = tempname()`

--- test/deprecated.jl
+++ test/deprecated.jl
@@ -1,8 +1,9 @@
 using DataFrames, FileIO, RDatasets, Compat
-fn = tempname()
-open(fn, "w") do f
-    redirect_stderr(f) do
-        dataset("datasets", "iris")
+mktemp() do fn, fio
+    open(fn, "w") do f
+        redirect_stderr(f) do
+            dataset("datasets", "iris")
+        end
     end
+    @test contains(readstring(fn), "WARNING: read_rda(args...) is deprecated, use FileIO.load(args...) instead.")
 end
-@test contains(readstring(fn), "WARNING: read_rda(args...) is deprecated, use FileIO.load(args...) instead.")
