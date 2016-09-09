ef36bd3941d3183d615294f42515cbd0b7f0bc64
Alex Arslan <ararslan@comcast.net>
2016-09-09 15:30:48 -0700

    Remove deprecation test (#1065)

--- test/deprecated.jl
+++
@@ -1,9 +0,0 @@
-using DataFrames, FileIO, RDatasets, Compat
-mktemp() do fn, fio
-    open(fn, "w") do f
-        redirect_stderr(f) do
-            dataset("datasets", "iris")
-        end
-    end
-    @test contains(readstring(fn), "WARNING: read_rda(args...) is deprecated, use FileIO.load(args...) instead.")
-end
