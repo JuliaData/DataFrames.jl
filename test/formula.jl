98b9e481627d0f7aa6ecb06b1f6d6aa52d466f96
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2016-10-03 15:35:12 -0400

    handle `A ~ B - 1` and add tests (#1086)
    * handle -1 and add tests
    
    * replace `import Base.==` with `Base.:(==)`
    
    * typo and error test

--- test/formula.jl
+++ test/formula.jl
@@ -53,9 +53,10 @@ module TestFormula
     @test t.intercept == false
     @test t.terms == [:x1, :x2]
 
-    t = Terms(y ~ -1 + x1 + x2)
-    @test t.intercept == false
-    @test t.terms == [:x1, :x2]
+    @test t == Terms(y ~ -1 + x1 + x2) == Terms(y ~ x1 - 1 + x2) == Terms(y ~ x1 + x2 -1)
+
+    ## can't subtract terms other than 1
+    @test_throws ErrorException Terms(y ~ x1 - x2)
 
     t = Terms(y ~ x1 & x2)
     @test t.terms == [:(x1 & x2)]
