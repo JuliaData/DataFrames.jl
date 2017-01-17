df6e516025f154b26e5f54b575916e58465b0c0c
Milan Bouchet-Valat <nalimilan@club.fr>
2017-01-17 22:23:30 +0100

    Fix computation of column width for Array{String} columns (#1145)
    These are rare enough to be untested, and Strings have a fast path which
    is different from Nullable{String}. The +2 must be a remnant from a time when
    we printed the quotes.
    
    By the way, fix indentation and a deprecation in the previous example, which
    is similar.

--- src/abstractdataframe/show.jl
+++ src/abstractdataframe/show.jl
@@ -37,7 +37,8 @@ let
         ourshowcompact(io, x)
         return position(io)
     end
-    ourstrwidth(x::AbstractString) = strwidth(x) + 2 # -> Int
+    ourstrwidth(x::AbstractString) = strwidth(x) # -> Int
+    myconv = VERSION < v"0.4-" ? convert : Base.unsafe_convert
     ourstrwidth(s::Symbol) =
         Int(ccall(:u8_strwidth,
                   Csize_t,
