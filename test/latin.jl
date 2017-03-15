dd91029859c901bac61de611ccd093d904a43e21
Amgad <amgad@peerialism.com>
2017-03-15 14:54:57 +0200

    fixed tests

--- test/latin.jl
+++ test/latin.jl
@@ -6,11 +6,15 @@ module TestIO
 
     data = joinpath(dirname(@__FILE__), "data")
 
+    # this fails for utf8 with exception
     df = readtable("$data/latin1/latin1.csv", encoding=:latin1)
     @test df[1, 2] == "Sí"
-
-    #fails for utf8
-    df = readtable("$data/latin1/latin1.csv")
-    @test df[1, 2] != "Sí"
+    
+    df = readtable("$data/latin1/latin_normal_headers.csv", encoding=:latin1)
+    @test df[1,2] == "Sí"
+    
+    # using utf it reads it incorrectly
+    df = readtable("$data/latin1/latin_normal_headers.csv", encoding=:latin1)
+    @test df[1,2] != "Sí"
     
 end
