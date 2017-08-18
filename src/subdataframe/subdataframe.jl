8a5f0ca309f58e1ba115dc5c3326ab3dbff82c74
rofinn <rory.finnegan@gmail.com>
2017-08-18 16:29:56 -0500

    Drop 0.5 `SubDataFrame` definition.

--- src/subdataframe/subdataframe.jl
+++ src/subdataframe/subdataframe.jl
@@ -5,39 +5,19 @@
 ##
 ##############################################################################
 
-if VERSION >= v"0.6.0-dev.2643"
-    include_string("""
-        immutable SubDataFrame{T <: AbstractVector{Int}} <: AbstractDataFrame
-            parent::DataFrame
-            rows::T # maps from subdf row indexes to parent row indexes
 
-            function SubDataFrame{T}(parent::DataFrame, rows::T) where {T <: AbstractVector{Int}}
-                if length(rows) > 0
-                    rmin, rmax = extrema(rows)
-                    if rmin < 1 || rmax > size(parent, 1)
-                        throw(BoundsError())
-                    end
-                end
-                new(parent, rows)
+immutable SubDataFrame{T <: AbstractVector{Int}} <: AbstractDataFrame
+    parent::DataFrame
+    rows::T # maps from subdf row indexes to parent row indexes
+
+    function SubDataFrame{T}(parent::DataFrame, rows::T) where {T <: AbstractVector{Int}}
+        if length(rows) > 0
+            rmin, rmax = extrema(rows)
+            if rmin < 1 || rmax > size(parent, 1)
+                throw(BoundsError())
             end
         end
-    """)
-else
-    @eval begin
-        immutable SubDataFrame{T <: AbstractVector{Int}} <: AbstractDataFrame
-            parent::DataFrame
-            rows::T # maps from subdf row indexes to parent row indexes
-
-            function SubDataFrame(parent::DataFrame, rows::T)
-                if length(rows) > 0
-                    rmin, rmax = extrema(rows)
-                    if rmin < 1 || rmax > size(parent, 1)
-                        throw(BoundsError())
-                    end
-                end
-                new(parent, rows)
-            end
-        end
+        new(parent, rows)
     end
 end
 
