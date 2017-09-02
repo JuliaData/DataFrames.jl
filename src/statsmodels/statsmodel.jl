JuliaData/DataTables.jl

Fix Julia 0.6 deprecations

src/statsmodels/statsmodel.jl
@@ -58,7 +58,7 @@ for (modeltype, dtmodeltype) in ((:StatisticalModel, DataTableStatisticalModel),
 end
 
 # Delegate functions from StatsBase that use our new types
-typealias DataTableModels @compat(Union{DataTableStatisticalModel, DataTableRegressionModel})
+@compat const DataTableModels = Union{DataTableStatisticalModel, DataTableRegressionModel}
 @delegate DataTableModels.model [StatsBase.coef, StatsBase.confint,
                                  StatsBase.deviance, StatsBase.nulldeviance,
                                  StatsBase.loglikelihood, StatsBase.nullloglikelihood,

JuliaData/DataFrames.jl

src/statsmodels/statsmodel.jl
@@ -58,7 +58,7 @@ for (modeltype, dfmodeltype) in ((:StatisticalModel, DataFrameStatisticalModel),
 end
 
 # Delegate functions from StatsBase that use our new types
-const DataFrameModels = Union{DataFrameStatisticalModel, DataFrameRegressionModel}
+@compat const DataFrameModels = Union{DataFrameStatisticalModel, DataFrameRegressionModel}
 @delegate DataFrameModels.model [StatsBase.coef, StatsBase.confint,
                                  StatsBase.deviance, StatsBase.nulldeviance,
                                  StatsBase.loglikelihood, StatsBase.nullloglikelihood,