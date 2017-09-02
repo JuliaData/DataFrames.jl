JuliaData/DataTables.jl

Cleanup views and subdatatable functions, add associated tests
The subdatatable/views code did not have a clear function heirarchy.
Sometimes view called subdatatable, sometimes subdatatable called back
up to view, and other times view would call view again before later
calling subdatatable. The code was also relying on a custom Index that
was used nowhere else, and seemed unneccessary. Fixes issue that users
could only specify single columns to subset on (rather than arrays of
columns), and adds tests for datatables and subdatatables to assert
view works as expected.

test/runtests.jl
@@ -23,7 +23,8 @@ my_tests = ["utils.jl",
             "join.jl",
             "iteration.jl",
             "duplicates.jl",
-            "show.jl"]
+            "show.jl",
+            "subdatatable.jl"]
 
 println("Running tests:")
 

JuliaData/DataFrames.jl

test/runtests.jl
@@ -23,7 +23,8 @@ my_tests = ["utils.jl",
             "join.jl",
             "iteration.jl",
             "duplicates.jl",
-            "show.jl"]
+            "show.jl",
+            "subdataframe.jl"]
 
 println("Running tests:")
 