8fc473d87345c01130dfd58ada4ef64c458808ba
Unknown <p.deffebach@gmail.com>
2018-07-08 15:02:37 -0500

    make REPL printing of `nothing` an `empty string`
    Also changes the name of `nastring` to `missingstring`

--- src/abstractdataframe/show.jl
+++ src/abstractdataframe/show.jl
@@ -250,6 +250,9 @@ function showrowindices(io::IO,
                 strlen = ourstrwidth(s)
                 if ismissing(s)
                     printstyled(io, s, color=:light_black)
+                elseif s == nothing
+                    ourshowcompact(io, "")
+                    strlen = 0
                 else
                     ourshowcompact(io, s)
                 end
