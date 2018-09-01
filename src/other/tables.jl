affc948f85825a3b0309ae396901c93d9cd55c04
quinnj <quinn.jacobd@gmail.com>
2018-09-01 07:58:06 -0600

    Updates for new Tables.jl updates

--- src/other/tables.jl
+++ src/other/tables.jl
@@ -1,10 +1,12 @@
 using Tables, IteratorInterfaceExtensions
 
-Tables.schema(df::DataFrame) = NamedTuple{Tuple(names(df)), Tuple{eltypes(df)...}}
-Tables.AccessStyle(df::DataFrame) = Tables.ColumnAccess()
-Tables.istable(::Type{DataFrame}) = true
+Tables.istable(::Type{<:AbstractDataFrame}) = true
+Tables.columnaccess(::Type{<:AbstractDataFrame}) = true
+Tables.columns(df::AbstractDataFrame) = df
+Tables.rowaccess(::Type{DataFrame}) = true
 Tables.rows(df::DataFrame) = eachrow(df)
-Tables.columns(df::DataFrame) = df
+
+Tables.schema(df::AbstractDataFrame) = Tables.Schema(names(df), eltypes(df))
 
 function _isiterabletable(x::T) where {T}
     Base.isiterable(T) || return false
@@ -13,20 +15,34 @@ end
 
 function DataFrame(x::T) where {T}
     if Tables.istable(T)
-        return DataFrame([collect(u) for u in Tables.columns(x)],
-                          collect(Tables.names(Tables.schema(x))))
+        columns = Tables.columns(x)
+        return DataFrame([collect(u) for u in Tables.eachcolumn(columns)],
+                          collect(Tables.schema(columns).names))
     end
     y = IteratorInterfaceExtensions.getiterator(x)
-    if _isiterabletable(y)
-        return DataFrame([collect(u) for u in Tables.buildcolumns(eltype(y), y)],
-                          collect(Tables.names(eltype(y))))
-    else
-        convert(DataFrame, x)
+    yT = typeof(y)
+    if Base.isiterable(yT)
+        if Base.IteratorEltype(yT) === Base.HasEltype() && eltype(y) <: NamedTuple
+            columns = Tables.buildcolumns(Tables.Schema(eltype(y)), y)
+            return DataFrame([collect(u) for u in columns],
+                        collect(Tables.schema(columns).names))
+        else
+            # non-NamedTuple or UnknownEltype
+            columns = Tables.buildcolumns(Tables.Schema(propertynames(first(x)), nothing), y)
+            return DataFrame([collect(u) for u in columns],
+                        collect(Tables.schema(columns).names))
+        end
     end
+    throw(ArgumentError("unable to construct DataFrame from $T"))
 end
 
-# needed to avoid ambiguities w/ another constructor; Tables.RowTable is just Vector{<:NamedTuple}
-DataFrame(x::Tables.RowTable) = DataFrame([collect(u) for u in Tables.columns(x)],
-                                          collect(Tables.names(Tables.schema(x))))
+Base.append!(df::DataFrame, x) = append!(df, DataFrame(x))
+
+# This supports the Tables.RowTable type; needed to avoid ambiguities w/ another constructor
+function DataFrame(x::Vector{T}) where {T <: NamedTuple}
+    columns = Tables.columns(x)
+    DataFrame([collect(u) for u in Tables.eachcolumn(columns)],
+               collect(Tables.schema(columns).names))
+end
 
 IteratorInterfaceExtensions.getiterator(df::DataFrame) = Tables.datavaluerows(df)
