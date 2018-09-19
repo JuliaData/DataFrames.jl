aabbc99cdb98bad86909dc51aa9f001567237064
quinnj <quinn.jacobd@gmail.com>
2018-09-19 08:14:31 -0600

    Remove make unique

--- src/other/tables.jl
+++ src/other/tables.jl
@@ -10,29 +10,29 @@ Tables.schema(df::AbstractDataFrame) = Tables.Schema(names(df), eltypes(df))
 
 getvector(x::AbstractVector) = x
 getvector(x) = collect(x)
-fromcolumns(x, makeunique) = DataFrame(Any[getvector(c) for c in Tables.eachcolumn(x)], collect(Symbol, propertynames(x)); makeunique=makeunique)
+fromcolumns(x) = DataFrame(Any[getvector(c) for c in Tables.eachcolumn(x)], Index(collect(Symbol, propertynames(x))))
 
-function DataFrame(x::T; makeunique::Bool=false) where {T}
-    if Tables.istable(T)
-        return fromcolumns(Tables.columns(x), makeunique)
+function DataFrame(x)
+    if Tables.istable(typeof(x))
+        return fromcolumns(Tables.columns(x))
     end
     it = TableTraits.isiterabletable(x)
     if it === true
         # Base.depwarn("constructing a DataFrame from an iterator is deprecated; $T should support the Tables.jl interface", nothing)
         y = IteratorInterfaceExtensions.getiterator(x)
-        return fromcolumns(Tables.columns(Tables.DataValueUnwrapper(y)), makeunique)
+        return fromcolumns(Tables.columns(Tables.DataValueUnwrapper(y)))
     elseif it === missing
         y = IteratorInterfaceExtensions.getiterator(x)
         # non-NamedTuple or EltypeUnknown
-        return fromcolumns(Tables.buildcolumns(nothing, Tables.DataValueUnwrapper(y)), makeunique)
+        return fromcolumns(Tables.buildcolumns(nothing, Tables.DataValueUnwrapper(y)))
     end
-    throw(ArgumentError("unable to construct DataFrame from $T"))
+    throw(ArgumentError("unable to construct DataFrame from $(typeof(x))"))
 end
 
 Base.append!(df::DataFrame, x) = append!(df, DataFrame(x))
 
 # This supports the Tables.RowTable type; needed to avoid ambiguities w/ another constructor
-DataFrame(x::Vector{T}; makeunique::Bool=false) where {T <: NamedTuple} = fromcolumns(Tables.columns(Tables.DataValueUnwrapper(x)), makeunique)
+DataFrame(x::Vector{T}) where {T <: NamedTuple} = fromcolumns(Tables.columns(Tables.DataValueUnwrapper(x)))
 
 IteratorInterfaceExtensions.getiterator(df::DataFrame) = Tables.datavaluerows(df)
 IteratorInterfaceExtensions.isiterable(x::DataFrame) = true
