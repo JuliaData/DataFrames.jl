c3236eee615191c244cb1b5510e682d30b9a6f92
Milan Bouchet-Valat <nalimilan@club.fr>
2018-09-22 13:21:41 +0200

    Avoid aliasing columns when assigning vector
    But avoid making a redundant copy for the first column.
    Also move related test out of test/index.jl, which is about testing the Index type
    rather than indexing.

--- src/dataframe/dataframe.jl
+++ src/dataframe/dataframe.jl
@@ -433,8 +433,9 @@ end
 function Base.setindex!(df::DataFrame,
                         v::AbstractVector,
                         col_inds::AbstractVector{<:ColumnIndex})
-    for col_ind in col_inds
-        df[col_ind] = v
+    df[col_inds[1]] = v
+    for col_ind in col_inds[2:end]
+        df[col_ind] = copy(v)
     end
     return df
 end
