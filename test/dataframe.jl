2dc245427b44e3566d20e99e92e21e844b6b0e59
Milan Bouchet-Valat <nalimilan@club.fr>
2018-09-24 18:13:21 +0200

    Improve tests

--- test/dataframe.jl
+++ test/dataframe.jl
@@ -735,12 +735,20 @@ module TestDataFrame
 
         # columns should not alias if vector assigned
         df = DataFrame(A=[0], B=[0])
-        df[1:end] = [0.0]
+        x = [0.0]
+        df[1:end] = x
+        x[1] = 1.0
+        @test df[1, :A] === 0.0
+        @test df[1, :B] === 0.0
         df[1, :A] = 1.0
         @test df[1, :B] === 0.0
 
         df = DataFrame(A=[0], B=[0])
-        df[:, 1:end] = [0.0]
+        x = [0.0]
+        df[:, 1:end] = x
+        x[1] = 1.0
+        @test df[1, :A] === 0.0
+        @test df[1, :B] === 0.0
         df[1, :A] = 1.0
         @test df[1, :B] === 0.0
     end
