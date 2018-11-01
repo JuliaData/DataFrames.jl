a57c1db382f841f1e8a04d825e36cbc8b597c37a
Bogumił Kamiński <bkamins@sgh.waw.pl>
2018-11-01 23:05:01 +0100

    fix makeunique test

--- test/utils.jl
+++ test/utils.jl
@@ -5,8 +5,7 @@ module TestUtils
     #
     @testset "make_unique" begin
         @test DataFrames.make_unique([:x, :x, :x_1, :x2], makeunique=true) == [:x, :x_2, :x_1, :x2]
-        # TODO uncomment this line after deprecation period when makeunique=false throws error
-        #@test_throws ArgumentError DataFrames.make_unique([:x, :x, :x_1, :x2], makeunique=false)
+        @test_throws ArgumentError DataFrames.make_unique([:x, :x, :x_1, :x2], makeunique=false)
         @test DataFrames.make_unique([:x, :x_1, :x2], makeunique=false) == [:x, :x_1, :x2]
     end
     #
