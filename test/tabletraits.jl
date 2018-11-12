8c5f112df06ef1b861d83eb9d830001163f7b976
David Anthoff <anthoff@berkeley.edu>
2018-11-12 00:43:18 -0800

    Add support for another TableTraits.jl interface (#1579)

---
+++ test/tabletraits.jl
@@ -0,0 +1,24 @@
+module TestTableTraits
+using Test, TableTraits, DataFrames
+
+struct ColumnSource
+end
+
+TableTraits.supports_get_columns_copy_using_missing(::ColumnSource) = true
+
+function TableTraits.get_columns_copy_using_missing(x::ColumnSource)
+    return (a=[1,2,3], b=[4.,5.,6.], c=["A", "B", "C"])
+end
+
+@testset "TableTraits" begin
+
+    df = DataFrame(ColumnSource())
+
+    @test size(df)==(3,3)
+    @test df.a==[1,2,3]
+    @test df.b==[4.,5.,6.]
+    @test df.c==["A", "B", "C"]
+
+end
+
+end
