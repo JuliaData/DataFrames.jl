3d65f7a76cba48f23c3c8b1df0f866459c5db9aa
Jacob Quinn <quinn.jacobd@gmail.com>
2018-11-13 23:02:34 -0700

    Adjust for Tables taking care of TableTraits logic

--- test/tabletraits.jl
+++ test/tabletraits.jl
@@ -5,6 +5,7 @@ struct ColumnSource
 end
 
 TableTraits.supports_get_columns_copy_using_missing(::ColumnSource) = true
+TableTraits.isiterabletable(::ColumnSource) = true
 
 function TableTraits.get_columns_copy_using_missing(x::ColumnSource)
     return (a=[1,2,3], b=[4.,5.,6.], c=["A", "B", "C"])
