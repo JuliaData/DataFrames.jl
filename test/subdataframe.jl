6875a5909e1ad730418ee5625b9af79cdd88173e
Milan Bouchet-Valat <nalimilan@club.fr>
2018-12-09 15:32:08 +0100

    Use ==

--- test/subdataframe.jl
+++ test/subdataframe.jl
@@ -119,7 +119,7 @@ module TestSubDataFrame
     @testset "dump" begin
         y = collect(1.0:10.0)
         df = view(DataFrame(y = y), 2:6, :)
-        @test sprint(dump, df) === """
+        @test sprint(dump, df) == """
                                    SubDataFrame{UnitRange{$Int}}  5 observations of 1 variables
                                      y: Array{Float64}((5,)) [2.0, 3.0, 4.0, 5.0, 6.0]
                                    """
