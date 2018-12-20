43e8378560ffd7fb3d21789300a0ec278e3c1d02
pdeffebach <p.deffebach@gmail.com>
2018-12-20 12:22:41 -0600

    Test fixes

--- src/groupeddataframe/show.jl
+++ src/groupeddataframe/show.jl
@@ -32,7 +32,7 @@ function Base.show(io::IO, gd::GroupedDataFrame;
             nrows = size(gd[1], 1)
             rows = nrows > 1 ? "rows" : "row"
                         
-            identified_groups = [':' * string(parent_names[col], " = ", first(gd[i][col])) 
+            identified_groups = [':' * string(parent_names[col], " = ", first(gd[1][col])) 
                                  for col in gd.cols]
 
             print(io, "\nFirst Group ($nrows $rows): ")
@@ -45,9 +45,8 @@ function Base.show(io::IO, gd::GroupedDataFrame;
             nrows = size(gd[N], 1)
             rows = nrows > 1 ? "rows" : "row"
             
-            identified_groups = [':' * string(parent_names[col], " = ", first(gd[i][col])) 
+            identified_groups = [':' * string(parent_names[col], " = ", first(gd[N][col])) 
                                  for col in gd.cols]
-                                  
             print(io, "\n⋮")
             print(io, "\nLast Group ($nrows $rows): ")
             join(io, identified_groups, ", ")
