3978895373e86851fb956ef564ab049d0162372c
pdeffebach <p.deffebach@gmail.com>
2018-12-20 12:19:20 -0600

    small fixes

---
+++ docs/src/lib/functions.md
@@ -0,0 +1,56 @@
+```@meta
+CurrentModule = DataFrames
+```
+
+# Functions
+
+```@index
+Pages = ["functions.md"]
+```
+
+## Grouping, Joining, and Split-Apply-Combine
+
+```@docs
+aggregate
+by
+colwise
+combine
+groupby
+join
+map
+melt
+stack
+unstack
+stackdf
+meltdf
+```
+
+## Basics
+
+```@docs
+allowmissing!
+completecases
+deletecols!
+deleterows!
+describe
+disallowmissing!
+dropmissing
+dropmissing!
+eachrow
+eachcol
+eltypes
+filter
+filter!
+insertcols!
+mapcols
+names!
+nonunique
+rename!
+rename
+repeat
+show
+sort
+sort!
+unique!
+permutecols!
+```
--- src/groupeddataframe/show.jl
+++ src/groupeddataframe/show.jl
@@ -18,9 +18,8 @@ function Base.show(io::IO, gd::GroupedDataFrame;
             nrows = size(gd[i], 1)
             rows = nrows > 1 ? "rows" : "row"
 
-            identified_groups = [':' * string(parent_names[col],
-                                 " = ",
-                                 first(gd[i][col])) for col in gd.cols]
+            identified_groups = [':' * string(parent_names[col], " = ", first(gd[i][col])) 
+                                 for col in gd.cols]
 
             print(io, "\nGroup $i ($nrows $rows): ")
             join(io, identified_groups, ", ")
@@ -33,9 +32,8 @@ function Base.show(io::IO, gd::GroupedDataFrame;
             nrows = size(gd[1], 1)
             rows = nrows > 1 ? "rows" : "row"
                         
-            identified_groups = [':' * string(parent_names[col],
-                                 " = ",
-                                 first(gd[1][col])) for col in gd.cols]
+            identified_groups = [':' * string(parent_names[col], " = ", first(gd[i][col])) 
+                                 for col in gd.cols]
 
             print(io, "\nFirst Group ($nrows $rows): ")
             join(io, identified_groups, ", ")
@@ -47,9 +45,9 @@ function Base.show(io::IO, gd::GroupedDataFrame;
             nrows = size(gd[N], 1)
             rows = nrows > 1 ? "rows" : "row"
             
-            identified_groups = [':' * string(parent_names[col],
-                                 " = ",
-                                 first(gd[N][col])) for col in gd.cols]
+            identified_groups = [':' * string(parent_names[col], " = ", first(gd[i][col])) 
+                                 for col in gd.cols]
+                                  
             print(io, "\n⋮")
             print(io, "\nLast Group ($nrows $rows): ")
             join(io, identified_groups, ", ")
