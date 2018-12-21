802180c5693ce06a23af535062766dcd4558b130
pdeffebach <peterwd@princeton.edu>
2018-12-20 21:05:52 -0600

    initial try

--- src/abstractdataframe/abstractdataframe.jl
+++ src/abstractdataframe/abstractdataframe.jl
@@ -1017,17 +1017,28 @@ julia> vcat(df1, df2)
 │ 6   │ 6     │ 6     │
 ```
 """
-Base.vcat(df::AbstractDataFrame) = df
-Base.vcat(dfs::AbstractDataFrame...) = _vcat(collect(dfs))
-function _vcat(dfs::AbstractVector{<:AbstractDataFrame})
+Base.vcat(df::AbstractDataFrame; widen::Bool = false, fillvalue = missing) = df
+Base.vcat(dfs::AbstractDataFrame...;  widen::Bool = false, fillvalue = missing) = _vcat(collect(dfs); widen = widen, fillvalue = fillvalue)
+function _vcat(dfs::AbstractVector{<:AbstractDataFrame}; widen::Bool = false, fillvalue = missing)
     isempty(dfs) && return DataFrame()
     @show allheaders = map(names, dfs)
     @show uniqueheaders = unique(allheaders)
     @show unionunique = union(uniqueheaders...)
     @show intersectunique = intersect(uniqueheaders...)
     @show coldiff = setdiff(unionunique, intersectunique)
+    # array of all headers
+    @show allheaders = map(names, dfs)
+    # unique arrays of all headers
+    @show uniqueheaders = unique(allheaders)
+    # Array of all the unique headers
+    @show unionunique = union(uniqueheaders...)
+    # Intersection of all unique headers 
+    @show intersectunique = intersect(uniqueheaders...)
+    # get the elements that are not present in everything
+    # one 
+    coldiff = setdiff(unionunique, intersectunique)
 
-    if !isempty(coldiff)
+    if (widen == false) && !isempty(coldiff)
         # if any DataFrames are a full superset of names, skip them
         filter!(u -> Set(u) != Set(unionunique), uniqueheaders)
         estrings = Vector{String}(undef, length(uniqueheaders))
@@ -1042,10 +1053,35 @@ function _vcat(dfs::AbstractVector{<:AbstractDataFrame})
     end
 
     @show header = allheaders[1]
+    # TODO: get preserve order of first headers as much 
+    # as possible 
+    # header = allheaders[1]
+    header = unionunique
     length(header) == 0 && return DataFrame()
     cols = Vector{AbstractVector}(undef, length(header))
     for (i, name) in enumerate(header)
         data = [df[name] for df in dfs]
+        # TODO: replace with commented out code after getindex deprecation
+        # data = [df[name] for df in dfs]
+        # the code below assumes that only DataFrame and SubDataFrame
+        # are subtypes of AbstractDataFrame
+        # it should be removed ASAP after deprecation
+        data = map(dfs) do df
+            if df isa DataFrame 
+                if haskey(df, name)
+                    return df[name] 
+                else
+                    return fill(fillvalue, nrow(df))
+                end
+            else 
+                if haskey(df, name)
+                    return view(parent(df)[name], rows(df))
+                else 
+                    return fill(fillvalue, length(rows(df)))
+                end
+            end
+        end
+
         lens = map(length, data)
         T = mapreduce(eltype, promote_type, data)
         cols[i] = Tables.allocatecolumn(T, sum(lens))
