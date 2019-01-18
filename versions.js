0e552d9c29a2d60645615bd3abca25a40b0e845d
autodocs <autodocs>
2019-01-18 20:52:46 +0000

    build based on d2ce979

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.17",
   "release-0.16",
   "release-0.15",
   "release-0.14",
@@ -8,6 +9,7 @@ var DOC_VERSIONS = [
   "release-0.12",
   "release-0.11",
   "release-0.10",
+  "v0.17.0",
   "v0.16.0",
   "v0.15.2",
   "v0.15.1",
