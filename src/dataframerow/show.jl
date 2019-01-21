eec04e7e037244635d08e4957ac3a6fc19ca2e1f
Ian Butterworth <i.r.butterworth@gmail.com>
2019-01-21 11:51:02 -0500

    add methods in dataframerow/show

--- src/dataframerow/show.jl
+++ src/dataframerow/show.jl
@@ -1,15 +1,17 @@
 function Base.show(io::IO, dfr::DataFrameRow;
                    allcols::Bool = !get(io, :limit, false),
                    splitcols = get(io, :limit, false),
-                   rowlabel::Symbol = :Row)
+                   rowlabel::Symbol = :Row,
+                   missingstring::AbstractString = "missing")
     r, c = parentindices(dfr)
     print(io, "DataFrameRow")
     _show(io, view(parent(dfr), [r], c), allcols=allcols, splitcols=splitcols,
-          rowlabel=rowlabel, summary=false, rowid=r)
+          rowlabel=rowlabel, summary=false, missingstring=missingstring, rowid=r)
 end
 
 Base.show(dfr::DataFrameRow;
           allcols::Bool = !get(io, :limit, true),
           splitcols = get(io, :limit, true),
-          rowlabel::Symbol = :Row) =
-    show(stdout, dfr, allcols=allcols, splitcols=splitcols, rowlabel=rowlabel)
+          rowlabel::Symbol = :Row,
+          missingstring::AbstractString = "missing") =
+    show(stdout, dfr, allcols=allcols, splitcols=splitcols, rowlabel=rowlabel, missingstring=missingstring)
