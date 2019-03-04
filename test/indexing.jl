838e45eb21f9724ab0c95935de3286b69a2fe843
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-03-04 10:41:15 +0100

    better eachcol test

--- test/indexing.jl
+++ test/indexing.jl
@@ -8,12 +8,13 @@ using Test, DataFrames
     @test df[1] == [1, 2, 3]
     @test df[1] === eachcol(df, false)[1]
     @test df[1:2] == DataFrame(a=1:3, b=4:6)
-    @test eachcol(view(df,1:2), false)[1] === eachcol(df, false)[1]
+    @test eachcol(df, false)[1] === df[1]
+    @test eachcol(view(df,1:2), false)[1] == eachcol(df, false)[1]
     @test eachcol(df[1:2], false)[1] == eachcol(df, false)[1]
     @test eachcol(df[1:2], false)[1] !== eachcol(df, false)[1]
     @test df[:] == df
     @test df[:] !== df
-    @test eachcol(view(df, :), false)[1] === eachcol(df, false)[1]
+    @test eachcol(view(df, :), false)[1] == eachcol(df, false)[1]
     @test eachcol(df[:], false)[1] == eachcol(df, false)[1]
     @test eachcol(df[:], false)[1] !== eachcol(df, false)[1]
 
