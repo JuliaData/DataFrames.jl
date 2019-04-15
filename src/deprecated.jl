0027f0e957c2268be8e518c22191cf03c517058d
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-04-15 20:11:35 +0200

    simplify deprecations

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -4,20 +4,18 @@ import Base: @deprecate
 
 @deprecate DataFrame(column_eltypes::AbstractVector{<:Type}, cnames::AbstractVector{Symbol},
                      nrows::Integer;
-                     makeunique::Bool=false) DataFrame(AbstractVector[elty >: Missing ?
-                                                                      fill!(Tables.allocatecolumn(elty, nrows), missing) :
-                                                                      Tables.allocatecolumn(elty, nrows)
-                                                                      for elty in column_eltypes],
-                                                       Index(convert(Vector{Symbol}, cnames),
-                                                             makeunique=makeunique),
+                     makeunique::Bool=false) DataFrame(AbstractVector[Vector{elty}(elty >: Missing ?
+                                                                                   missing : undef, nrows)
+                                                              for elty in column_eltypes],
+                                                       convert(Vector{Symbol}, cnames),
+                                                       makeunique=makeunique,
                                                        copycols=false)
 
 @deprecate DataFrame(column_eltypes::AbstractVector{<:Type},
-                     nrows::Integer) DataFrame(AbstractVector[elty >: Missing ?
-                                                              fill!(Tables.allocatecolumn(elty, nrows), missing) :
-                                                              Tables.allocatecolumn(elty, nrows)
+                     nrows::Integer) DataFrame(AbstractVector[Vector{elty}(elty >: Missing ?
+                                                                           missing : undef, nrows)
                                                               for elty in column_eltypes],
-                                               Index(gennames(length(column_eltypes))),
+                                               gennames(length(column_eltypes)),
                                                copycols=false)
 
 function DataFrame(column_eltypes::AbstractVector{T}, cnames::AbstractVector{Symbol},
