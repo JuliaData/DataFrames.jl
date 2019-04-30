982bf5c22e8dcbfd3021399c15e63718785f377f
pdeffebach <p.deffebach@gmail.com>
2019-04-30 14:35:05 -0500

    Actually add back deleted sentence

--- src/groupeddataframe/grouping.jl
+++ src/groupeddataframe/grouping.jl
@@ -242,9 +242,6 @@ When computing the `sum` or `mean` over floating point columns, results will be 
 accurate than the standard [`sum`](@ref) function (which uses pairwise summation). Use
 `col => x -> sum(x)` to avoid the optimized method and use the slower, more accurate one.
 
-`by(d, cols, f)` is equivalent to `combine(f, groupby(d, cols))` and to the
-less efficient `combine(map(f, groupby(d, cols)))`.
-
 ### Examples
 
 ```jldoctest
@@ -1018,6 +1015,8 @@ When computing the `sum` or `mean` over floating point columns, results will be 
 accurate than the standard [`sum`](@ref) function (which uses pairwise summation). Use
 `col => x -> sum(x)` to avoid the optimized method and use the slower, more accurate one.
 
+`by(d, cols, f)` is equivalent to `combine(f, groupby(d, cols))` and to the
+less efficient `combine(map(f, groupby(d, cols)))`.
 
 ### Examples
 
