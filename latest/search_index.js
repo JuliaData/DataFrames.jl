adb30fca247f6d0ac4bf23e9114effa9e489c12a
autodocs <autodocs>
2019-06-08 13:30:44 +0000

    build based on 514017d

--- latest/search_index.js
+++ latest/search_index.js
@@ -568,4 +568,12 @@ var documenterSearchIndex = {"docs": [
     "text": "Under construction"
 },
 
+{
+    "location": "lib/indexing.html#Broadcasting-1",
+    "page": "Indexing",
+    "title": "Broadcasting",
+    "category": "section",
+    "text": "It is possible to assign a value to AbstractDataFrame and DataFrameRow objects using the .= operator. In such an operation AbstractDataFrame is considered as two-dimensional and DataFrameRow as single-dimensional.note: Note\nThe rule above means that, similar to single-dimensional objects in Base (e.g. vectors), DataFrameRow is considered to be column-oriented.If column indexing using Symbol names is performed the order of columns in the operation is specified by the order of names.df[col] .= value is allowed when col is a Symbol even if col is not present in the DataFrame under the condition that df is not empty: a new column will be created. On the contrary, df.col .= value is not allowed if col is not present in df."
+},
+
 ]}
