3ab8bd2156134f7bcaf25f2050c635d6790ca389
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-08-25 14:47:08 -0400

    Add `@ref` links to `groupby` docstring

--- src/groupeddataframe/grouping.jl
+++ src/groupeddataframe/grouping.jl
@@ -61,10 +61,10 @@ and combines the result into a data frame).
 
 See the following for additional split-apply-combine operations:
 
-* `by` : split-apply-combine using functions
-* `aggregate` : split-apply-combine; applies functions in the form of a cross product
-* `map` : apply a function to each group of a `GroupedDataFrame` (without combining)
-* `combine` : combine a `GroupedDataFrame`, optionally applying a function to each group
+* [`by`](@ref) : split-apply-combine using functions
+* [`aggregate`](@ref) : split-apply-combine; applies functions in the form of a cross product
+* [`map`](@ref) : apply a function to each group of a `GroupedDataFrame` (without combining)
+* [`combine`](@ref) : combine a `GroupedDataFrame`, optionally applying a function to each group
 
 ### Examples
 
