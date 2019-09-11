f1af67c6b3760e9bd8da0053a570e9397da69de4
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-09-12 00:18:00 +0200

    fix equality test

--- test/dataframe.jl
+++ test/dataframe.jl
@@ -1334,9 +1334,9 @@ end
     end
 
     @test_throws MethodError disallowmissing(DataFrame(x=[missing]))
-    @test disallowmissing(DataFrame(x=[missing]), skip=true) == DataFrame(x=[missing])
+    @test disallowmissing(DataFrame(x=[missing]), skip=true) ≅ DataFrame(x=[missing])
     @test_throws MethodError disallowmissing(DataFrame(x=[1, missing]))
-    @test disallowmissing(DataFrame(x=[1, missing]), skip=true) == DataFrame(x=[1, missing])
+    @test disallowmissing(DataFrame(x=[1, missing]), skip=true) ≅ DataFrame(x=[1, missing])
 
     df = DataFrame(x=[1], y = Union{Int,Missing}[1], z=[missing])
     df2 = disallowmissing(df, skip=true)
