63263f1b26909f8f81da04306fc80016e92d8870
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-09-19 09:18:30 +0200

    fix deprecation

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1532,8 +1532,8 @@ function Base.setindex!(df::DataFrame, v, ::Colon, col_inds)
     df
 end
 
-function Base.maybeview(df::AbstractDataFrame, col::ColumnIndex)
-    Base.depwarn("in broadcasted assignment use `df[:, col]` instead of `df[col]`", :maybeview)
+function Base.dotview(df::AbstractDataFrame, col::ColumnIndex)
+    Base.depwarn("in broadcasted assignment use `df[:, col]` instead of `df[col]`", :dotview)
     @view df[:, col]
 end
 
