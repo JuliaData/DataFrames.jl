a8247a3ac4655c306ecd87a458bcde4fd74c97b1
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-11-26 00:12:22 +0100

    refactor Index tests

--- test/index.jl
+++ test/index.jl
@@ -3,169 +3,176 @@ module TestIndex
 using Test, DataFrames
 using DataFrames: Index, SubIndex, fuzzymatch
 
-i = Index()
-push!(i, :A)
-push!(i, :B)
+@testset "Index indexing" begin
+    i = Index()
+    push!(i, :A)
+    push!(i, :B)
 
-inds = Any[1,
-           big(1),
-           :A,
-           [true, false],
-           [1],
-           [big(1)],
-           big(1):big(1),
-           [:A],
-           Union{Int, Missing}[1],
-           Union{BigInt, Missing}[big(1)],
-           Union{Symbol, Missing}[:A],
-           Any[1],
-           Any[:A]]
+    inds = Any[1,
+               big(1),
+               :A,
+               [true, false],
+               [1],
+               [big(1)],
+               big(1):big(1),
+               [:A],
+               Union{Int, Missing}[1],
+               Union{BigInt, Missing}[big(1)],
+               Union{Symbol, Missing}[:A],
+               Any[1],
+               Any[:A]]
 
-for ind in inds
-    if ind == :A || ndims(ind) == 0
-        @test i[ind] == 1
-    else
-        @test (i[ind] == [1])
+    for ind in inds
+        if ind == :A || ndims(ind) == 0
+            @test i[ind] == 1
+        else
+            @test (i[ind] == [1])
+        end
+    end
+
+    @test_throws MethodError i[1.0]
+    @test_throws ArgumentError i[true]
+    @test_throws ArgumentError i[false]
+    @test_throws ArgumentError i[Union{Bool, Missing}[true, false]]
+    @test_throws ArgumentError i[Any[1, missing]]
+    @test_throws ArgumentError i[[1, missing]]
+    @test_throws ArgumentError i[[true, missing]]
+    @test_throws ArgumentError i[Any[true, missing]]
+    @test_throws MethodError i[[:A, missing]]
+    @test_throws MethodError i[Any[:A, missing]]
+    @test_throws ArgumentError i[1.0:1.0]
+    @test_throws ArgumentError i[[1.0]]
+    @test_throws ArgumentError i[Any[1.0]]
+    @test_throws BoundsError i[0]
+    @test_throws BoundsError i[10]
+    @test_throws ArgumentError i[:x]
+    @test_throws BoundsError i[1:3]
+    @test_throws ArgumentError i[[1,1]]
+    @test_throws ArgumentError i[[:A,:A]]
+    @test_throws BoundsError i[Not(0)]
+    @test_throws BoundsError i[Not(10)]
+    @test_throws ArgumentError i[Not(:x)]
+    @test_throws BoundsError i[Not(1:3)]
+    @test_throws ArgumentError i[Not([1,1])]
+    @test_throws ArgumentError i[Not([:A,:A])]
+
+    @test i[1:1] == 1:1
+
+    @test_throws BoundsError i[[true]]
+    @test_throws BoundsError i[true:true]
+    @test_throws BoundsError i[[true, false, true]]
+
+    @test_throws ArgumentError i[["a"]]
+    @test_throws ArgumentError i[Any["a"]]
+
+    @test i[[]] == Int[]
+    @test i[Int[]] == Int[]
+    @test i[Symbol[]] == Int[]
+    @test i[:] == 1:length(i)
+    @test i[Not(Not(:))] == 1:length(i)
+    @test i[Not(1:0)] == 1:length(i)
+end
+
+@testset "rename!" begin
+    i = Index([:A,:B])
+    @test names(i) == [:A,:B]
+    @test rename!(i, [:a,:a], makeunique=true) == Index([:a,:a_1])
+    @test_throws ArgumentError rename!(i, [:a,:a])
+    @test rename!(copy(i), [:a,:b]) == Index([:a,:b])
+    @test names(i) == [:a,:a_1]
+    @test rename!(i, [:a,:b]) == Index([:a,:b])
+    @test rename!(copy(i), Dict(:a=>:A, :b=>:B)) == Index([:A,:B])
+    @test rename!(copy(i), :a => :A) == Index([:A,:b])
+    @test rename!(copy(i), :a => :a) == Index([:a,:b])
+    @test rename!(copy(i), [:a => :A]) == Index([:A,:b])
+    @test rename!(copy(i), [:a => :a]) == Index([:a,:b])
+    @test rename!(x->Symbol(uppercase(string(x))), copy(i)) == Index([:A,:B])
+    @test rename!(x->Symbol(lowercase(string(x))), copy(i)) == Index([:a,:b])
+
+    @test delete!(i, :a) == Index([:b])
+    push!(i, :C)
+    @test delete!(i, 1) == Index([:C])
+
+    i = Index([:A, :B, :C, :D, :E])
+    i2 = copy(i)
+    rename!(i2, reverse(names(i2)))
+    rename!(i2, reverse(names(i2)))
+    @test names(i2) == names(i)
+    for name in names(i)
+      i2[name] # Issue #715
     end
 end
 
-@test_throws MethodError i[1.0]
-@test_throws ArgumentError i[true]
-@test_throws ArgumentError i[false]
-@test_throws ArgumentError i[Union{Bool, Missing}[true, false]]
-@test_throws ArgumentError i[Any[1, missing]]
-@test_throws ArgumentError i[[1, missing]]
-@test_throws ArgumentError i[[true, missing]]
-@test_throws ArgumentError i[Any[true, missing]]
-@test_throws MethodError i[[:A, missing]]
-@test_throws MethodError i[Any[:A, missing]]
-@test_throws ArgumentError i[1.0:1.0]
-@test_throws ArgumentError i[[1.0]]
-@test_throws ArgumentError i[Any[1.0]]
-@test_throws BoundsError i[0]
-@test_throws BoundsError i[10]
-@test_throws ArgumentError i[:x]
-@test_throws BoundsError i[1:3]
-@test_throws ArgumentError i[[1,1]]
-@test_throws ArgumentError i[[:A,:A]]
-@test_throws BoundsError i[Not(0)]
-@test_throws BoundsError i[Not(10)]
-@test_throws ArgumentError i[Not(:x)]
-@test_throws BoundsError i[Not(1:3)]
-@test_throws ArgumentError i[Not([1,1])]
-@test_throws ArgumentError i[Not([:A,:A])]
+@testset "SubIndex" begin
+    i = Index([:A, :B, :C, :D, :E])
+    si1 = SubIndex(i, :)
+    si2 = SubIndex(i, 3:5)
+    si3 = SubIndex(i, [3,4,5])
+    si4 = SubIndex(i, [false, false, true, true, true])
+    si5 = SubIndex(i, [:C, :D, :E])
+    si6 = SubIndex(i, Not(Not([:C, :D, :E])))
+    si7 = SubIndex(i, Not(1:2))
 
-@test i[1:1] == 1:1
+    @test copy(si1) == i
+    @test copy(si2) == Index([:C, :D, :E])
+    @test copy(si3) == Index([:C, :D, :E])
+    @test copy(si4) == Index([:C, :D, :E])
+    @test copy(si5) == Index([:C, :D, :E])
+    @test copy(si6) == Index([:C, :D, :E])
+    @test copy(si7) == Index([:C, :D, :E])
 
-@test_throws BoundsError i[[true]]
-@test_throws BoundsError i[true:true]
-@test_throws BoundsError i[[true, false, true]]
+    @test_throws ArgumentError SubIndex(i, 1)
+    @test_throws ArgumentError SubIndex(i, :A)
+    @test_throws ArgumentError SubIndex(i, true)
+    @test si1 isa Index
+    @test si2.cols == 3:5
+    @test si2.remap == -1:3
+    @test si3.cols == 3:5
+    @test si3.remap == Int[]
+    @test !haskey(si3, :A)
+    @test si3.remap == [0, 0, 1, 2, 3]
+    @test si4.cols == 3:5
+    @test si4.remap == Int[]
+    @test !haskey(si4, :A)
+    @test si4.remap == [0, 0, 1, 2, 3]
+    @test si5.cols == 3:5
+    @test si5.remap == Int[]
+    @test !haskey(si5, :A)
+    @test si5.remap == [0, 0, 1, 2, 3]
+    @test si6.cols == 3:5
+    @test si6.remap == Int[]
+    @test !haskey(si6, :A)
+    @test si6.remap == [0, 0, 1, 2, 3]
+    @test si7.cols == 3:5
+    @test si7.remap == Int[]
+    @test !haskey(si7, :A)
+    @test si7.remap == [0, 0, 1, 2, 3]
 
-@test_throws ArgumentError i[["a"]]
-@test_throws ArgumentError i[Any["a"]]
+    @test length(si1) == 5
+    @test length(si2) == 3
+    @test length(si3) == 3
+    @test length(si4) == 3
+    @test length(si5) == 3
+    @test length(si6) == 3
+    @test length(si7) == 3
 
-@test i[[]] == Int[]
-@test i[Int[]] == Int[]
-@test i[Symbol[]] == Int[]
-@test i[:] == 1:length(i)
-@test i[Not(Not(:))] == 1:length(i)
-@test i[Not(1:0)] == 1:length(i)
+    @test names(si1) == keys(si1) == [:A, :B, :C, :D, :E]
+    @test names(si2) == keys(si2) == [:C, :D, :E]
+    @test names(si3) == keys(si3) == [:C, :D, :E]
+    @test names(si4) == keys(si4) == [:C, :D, :E]
+    @test names(si5) == keys(si5) == [:C, :D, :E]
+    @test names(si6) == keys(si5) == [:C, :D, :E]
+    @test names(si7) == keys(si5) == [:C, :D, :E]
 
-@test names(i) == [:A,:B]
-@test rename!(i, [:a,:a], makeunique=true) == Index([:a,:a_1])
-@test_throws ArgumentError rename!(i, [:a,:a])
-@test rename!(copy(i), [:a,:b]) == Index([:a,:b])
-@test names(i) == [:a,:a_1]
-@test rename!(i, [:a,:b]) == Index([:a,:b])
-@test rename!(copy(i), Dict(:a=>:A, :b=>:B)) == Index([:A,:B])
-@test rename!(copy(i), :a => :A) == Index([:A,:b])
-@test rename!(copy(i), :a => :a) == Index([:a,:b])
-@test rename!(copy(i), [:a => :A]) == Index([:A,:b])
-@test rename!(copy(i), [:a => :a]) == Index([:a,:b])
-@test rename!(x->Symbol(uppercase(string(x))), copy(i)) == Index([:A,:B])
-@test rename!(x->Symbol(lowercase(string(x))), copy(i)) == Index([:a,:b])
-
-@test delete!(i, :a) == Index([:b])
-push!(i, :C)
-@test delete!(i, 1) == Index([:C])
-
-i = Index([:A, :B, :C, :D, :E])
-i2 = copy(i)
-rename!(i2, reverse(names(i2)))
-rename!(i2, reverse(names(i2)))
-@test names(i2) == names(i)
-for name in names(i)
-  i2[name] # Issue #715
+    @test_throws ArgumentError haskey(si3, true)
+    @test haskey(si3, 1)
+    @test !haskey(si3, 0)
+    @test !haskey(si3, 4)
+    @test haskey(si3, :D)
+    @test !haskey(si3, :A)
+    @test si3[:C] == 1
+    @test si3[names(i)] == [0, 0, 1, 2, 3]
 end
-
-i = Index([:A, :B, :C, :D, :E])
-si1 = SubIndex(i, :)
-si2 = SubIndex(i, 3:5)
-si3 = SubIndex(i, [3,4,5])
-si4 = SubIndex(i, [false, false, true, true, true])
-si5 = SubIndex(i, [:C, :D, :E])
-si6 = SubIndex(i, Not(Not([:C, :D, :E])))
-si7 = SubIndex(i, Not(1:2))
-
-@test copy(si1) == i
-@test copy(si2) == Index([:C, :D, :E])
-@test copy(si3) == Index([:C, :D, :E])
-@test copy(si4) == Index([:C, :D, :E])
-@test copy(si5) == Index([:C, :D, :E])
-@test copy(si6) == Index([:C, :D, :E])
-@test copy(si7) == Index([:C, :D, :E])
-
-@test_throws ArgumentError SubIndex(i, 1)
-@test_throws ArgumentError SubIndex(i, :A)
-@test_throws ArgumentError SubIndex(i, true)
-@test si1 isa Index
-@test si2.cols == 3:5
-@test si2.remap == -1:3
-@test si3.cols == 3:5
-@test si3.remap == Int[]
-@test !haskey(si3, :A)
-@test si3.remap == [0, 0, 1, 2, 3]
-@test si4.cols == 3:5
-@test si4.remap == Int[]
-@test !haskey(si4, :A)
-@test si4.remap == [0, 0, 1, 2, 3]
-@test si5.cols == 3:5
-@test si5.remap == Int[]
-@test !haskey(si5, :A)
-@test si5.remap == [0, 0, 1, 2, 3]
-@test si6.cols == 3:5
-@test si6.remap == Int[]
-@test !haskey(si6, :A)
-@test si6.remap == [0, 0, 1, 2, 3]
-@test si7.cols == 3:5
-@test si7.remap == Int[]
-@test !haskey(si7, :A)
-@test si7.remap == [0, 0, 1, 2, 3]
-
-@test length(si1) == 5
-@test length(si2) == 3
-@test length(si3) == 3
-@test length(si4) == 3
-@test length(si5) == 3
-@test length(si6) == 3
-@test length(si7) == 3
-
-@test names(si1) == keys(si1) == [:A, :B, :C, :D, :E]
-@test names(si2) == keys(si2) == [:C, :D, :E]
-@test names(si3) == keys(si3) == [:C, :D, :E]
-@test names(si4) == keys(si4) == [:C, :D, :E]
-@test names(si5) == keys(si5) == [:C, :D, :E]
-@test names(si6) == keys(si5) == [:C, :D, :E]
-@test names(si7) == keys(si5) == [:C, :D, :E]
-
-@test_throws ArgumentError haskey(si3, true)
-@test haskey(si3, 1)
-@test !haskey(si3, 0)
-@test !haskey(si3, 4)
-@test haskey(si3, :D)
-@test !haskey(si3, :A)
-@test si3[:C] == 1
-@test si3[names(i)] == [0, 0, 1, 2, 3]
 
 @testset "selector mutation" begin
     df = DataFrame(a=1:5, b=11:15, c=21:25)
