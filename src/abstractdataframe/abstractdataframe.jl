bda218b055ab21aa0794e9b0f8e232cbf4903261
Bogumił Kamiński <bkamins@sgh.waw.pl>
2019-12-06 17:17:09 +0100

    fix docs

--- src/abstractdataframe/abstractdataframe.jl
+++ src/abstractdataframe/abstractdataframe.jl
@@ -95,8 +95,7 @@ then:
 * `from` value can be a `Symbol`, an `AbstractString` or an `Integer`;
 * `to` value can be a `Symbol` or an `AbstractString`.
 
-It is required that the type of `from` and of `to` values is the same (e.g.
-it is not allowed to mix strings and symbols as `from` values).
+Mixing symbols and strings in `to` and `from` is not allowed.
 
 See also: [`rename`](@ref)
 
@@ -129,6 +128,15 @@ ERROR: ArgumentError: Duplicate variable names: :a. Pass makeunique=true to make
 julia> rename(df, [:a, :b, :a], makeunique=true)
 1×3 DataFrame
 │ Row │ a     │ b     │ a_1   │
+│     │ Int64 │ Int64 │ Int64 │
+├─────┼───────┼───────┼───────┤
+│ 1   │ 1     │ 2     │ 3     │
+
+julia> rename!(df) do x
+           uppercase(string(x))
+       end
+1×3 DataFrame
+│ Row │ A     │ B     │ A_1   │
 │     │ Int64 │ Int64 │ Int64 │
 ├─────┼───────┼───────┼───────┤
 │ 1   │ 1     │ 2     │ 3     │
@@ -180,37 +188,36 @@ function rename!(f::Function, df::AbstractDataFrame)
 end
 
 """
-    rename!(df::AbstractDataFrame, vals::AbstractVector{Symbol}; makeunique::Bool=false)
-    rename!(df::AbstractDataFrame, vals::AbstractVector{AbstractString}; makeunique::Bool=false)
-    rename!(df::AbstractDataFrame, (from => to)::Pair...)
-    rename!(df::AbstractDataFrame, d::AbstractDict)
-    rename!(df::AbstractDataFrame, d::AbstractArray{Pair})
-    rename!(f::Function, df::AbstractDataFrame)
+    rename(df::AbstractDataFrame, vals::AbstractVector{Symbol}; makeunique::Bool=false)
+    rename(df::AbstractDataFrame, vals::AbstractVector{<:AbstractString}; makeunique::Bool=false)
+    rename(df::AbstractDataFrame, (from => to)::Pair...)
+    rename(df::AbstractDataFrame, d::AbstractDict)
+    rename(df::AbstractDataFrame, d::AbstractArray{<:Pair})
+    rename(f::Function, df::AbstractDataFrame)
 
 Create a new data frame that is a copy of `df` with changed column names.
 Each name is changed at most once. Permutation of names is allowed.
 
 # Arguments
 - `df` : the `AbstractDataFrame`
-- `d` : an `AbstractDict` type or an `AbstractVector` of `Pairs` that maps
+- `d` : an `AbstractDict` or an `AbstractVector` of `Pair`s that maps
   the original names or column numbers to new names
 - `f` : a function which for each column takes the old name (a `Symbol`)
   and returns the new name that gets converted to a `Symbol`
-- `vals` : new column names as a vector of `Symbol`s or `AbstractStrings`
+- `vals` : new column names as a vector of `Symbol`s or `AbstractString`s
   of the same length as the number of columns in `df`
 - `makeunique` : if `false` (the default), an error will be raised
   if duplicate names are found; if `true`, duplicate names will be suffixed
   with `_i` (`i` starting at 1 for the first duplicate).
 
-If pairs are passed to `rename!` (as positional arguments or by a dictionary or a vector)
+If pairs are passed to `rename` (as positional arguments or in a dictionary or a vector)
 then:
 * `from` value can be a `Symbol`, an `AbstractString` or an `Integer`;
-* `to` value can be a `Symbol`, an `AbstractString`.
+* `to` value can be a `Symbol` or an `AbstractString`.
 
-It is required that the type of `from` and of `to` values is the same (e.g.
-it is not allowed to mix strings and symbols as `from` values).
+Mixing symbols and strings in `to` and `from` is not allowed.
 
-See also: [`rename`](@ref)
+See also: [`rename!`](@ref)
 
 # Examples
 ```julia
