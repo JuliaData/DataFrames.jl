05f4e7c1745d847ba56d865b41bde58bb74a37d5
Takafumi Arakaki <aka.tkf@gmail.com>
2019-12-20 00:36:56 -0800

    Test Tables interface with view(df, :, :)

--- test/tables.jl
+++ test/tables.jl
@@ -56,7 +56,12 @@ Base.propertynames(d::DuplicateNamesColumnTable) = (:a, :a, :b)
 @testset "Tables" begin
     df = DataFrame(a=Int64[1, 2, 3], b=[:a, :b, :c])
 
-    @testset "basics $(nameof(typeof(table)))" for table in [df, eachrow(df), eachcol(df)]
+    @testset "basics $(nameof(typeof(table)))" for table in [
+        df,
+        view(df, :, :),
+        eachrow(df),
+        eachcol(df),
+    ]
         @test Tables.istable(table)
         @test Tables.rowaccess(table)
         @test Tables.columnaccess(table)
