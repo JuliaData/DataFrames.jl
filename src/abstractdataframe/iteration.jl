65c969d99eac610f612819f7fa4fad56db550ead
Bogumił Kamiński <bkamins@sgh.waw.pl>
2020-06-24 18:01:49 +0200

    Apply suggestions from code review
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/abstractdataframe/iteration.jl
+++ src/abstractdataframe/iteration.jl
@@ -110,16 +110,16 @@ const DATAFRAMECOLUMNS_DOCSTR = """
 Indexing into `DataFrameColumns` objects using integer, `Symbol` or string
 returns the corresponding column (without copying).
 Indexing into `DataFrameColumns` objects using a multiple column selector
-returns a subsetted `DataFrameColumns` object with parent containing only the
-selected columns (without copying).
+returns a subsetted `DataFrameColumns` object with a new parent containing
+only the selected columns (without copying).
 
 `DataFrameColumns` supports most of the `AbstractVector` API. The key
 differences are that it is read-only and that the `keys` function returns a
 vector of `Symbol`s (and not integers as for normal vectors).
 
 In particular `findnext`, `findprev`, `findfirst`, `findlast`, and `findall`
-functions are supported, and in `findnext`, `findprev` functions it is allowed
-to pass integer, string, or `Symbol` as a reference index.
+functions are supported, and in `findnext` and `findprev` functions it is allowed
+to pass an integer, string, or `Symbol` as a reference index.
 """
 
 """
