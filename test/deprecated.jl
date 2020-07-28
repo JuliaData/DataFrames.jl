2f991d8a6f6499d3220fc780e8ad76c396443a27
Bogumił Kamiński <bkamins@sgh.waw.pl>
2020-07-28 11:11:36 +0200

    add deprecated tests

--- test/deprecated.jl
+++ test/deprecated.jl
@@ -707,6 +707,55 @@ end
     end
 end
 
+@testset "DataFrame!" begin
+    @test_throws ArgumentError DataFrame!(x=x, y=y, copycols=true)
+
+    x = [1,2,3]
+    y = [4,5,6]
+    df2 = DataFrame!(df1)
+    @test df1 == df2
+    @test df1.x === df2.x
+    @test df1.y === df2.y
+
+    a=[1,2,3]
+    df = DataFrame!(:a=>a, :b=>1, :c=>1:3)
+    @test propertynames(df) == [:a, :b, :c]
+    @test df.a === a
+
+    df = DataFrame!("a"=>a, "b"=>1, "c"=>1:3)
+    @test propertynames(df) == [:a, :b, :c]
+    @test df."a" === a
+
+    df = DataFrame!(Dict(:a=>a, :b=>1, :c=>1:3))
+    @test propertynames(df) == [:a, :b, :c]
+    @test df.a === a
+
+    df = DataFrame!(Dict("a"=>a, "b"=>1, "c"=>1:3))
+    @test propertynames(df) == [:a, :b, :c]
+    @test df."a" === a
+
+    df = DataFrame!((x, y))
+    @test propertynames(df) == [:x1, :x2]
+    @test df.x1 === x
+    @test df.x2 === y
+
+    df = DataFrame!((x, y), (:x1, :x2))
+    @test propertynames(df) == [:x1, :x2]
+    @test df.x1 === x
+    @test df.x2 === y
+
+    df = DataFrame!((x, y), ("x1", "x2"))
+    @test names(df) == ["x1", "x2"]
+    @test df."x1" === x
+    @test df."x2" === y
+
+    @test_throws MethodError DataFrame!([1 2; 3 4], copycols=false)
+    @test_throws ArgumentError DataFrame!([1 2; 3 4])
+
+    @test_throws ArgumentError DataFrame!([Union{Int, Missing}, Union{Float64, Missing}],
+                                      [:x1, :x2], 2)
+end
+
 global_logger(old_logger)
 
 end # module
