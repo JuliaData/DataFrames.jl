e88184576e9deaf281eda9e6741a7033b482bd27
Bogumił Kamiński <bkamins@sgh.waw.pl>
2020-08-06 13:23:53 +0200

    revert by and aggregate

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,3 +1,10 @@
 import Base: @deprecate
 
 @deprecate DataFrame!(args...; kwargs...) DataFrame(args...; copycols=false, kwargs...)
+
+# TODO: remove these definitions in year 2021
+by(args...; kwargs...) = throw(ArgumentError("by function was removed from DataFrames.jl. " *
+                                             "Use the `groupby` and `combine` functions instead."))
+
+aggregate(args...; kwargs...) = throw(ArgumentError("aggregate function was removed from DataFrames.jl. " *
+                                                    "Use the `combine` functions instead."))
