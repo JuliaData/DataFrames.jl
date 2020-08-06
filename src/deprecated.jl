d78b4cc11fc1347c766c8dafc11c7e19dfa07037
Bogumił Kamiński <bkamins@sgh.waw.pl>
2020-08-06 19:01:46 +0200

    Apply suggestions from code review
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -4,7 +4,7 @@ import Base: @deprecate
 
 # TODO: remove these definitions in year 2021
 by(args...; kwargs...) = throw(ArgumentError("by function was removed from DataFrames.jl. " *
-                                             "Use the `groupby` and `combine` functions instead."))
+                                             "Use the `combine(groupby(...), ...)` or `combine(f, groupby(...))` instead."))
 
 aggregate(args...; kwargs...) = throw(ArgumentError("aggregate function was removed from DataFrames.jl. " *
-                                                    "Use the `combine` functions instead."))
+                                                    "Use the `combine` function instead."))
