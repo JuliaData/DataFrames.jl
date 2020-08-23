000864fd8c1d2415d5a604a8c2b92e6960546453
Arsh Sharma <43717431+Sov-trotter@users.noreply.github.com>
2020-08-23 19:27:13 +0530

    Add isapprox support for dataframes (#2373)

--- src/abstractdataframe/abstractdataframe.jl
+++ src/abstractdataframe/abstractdataframe.jl
@@ -410,6 +410,22 @@ function Base.isequal(df1::AbstractDataFrame, df2::AbstractDataFrame)
     return true
 end
 
+"""
+    isapprox(df1::AbstractDataFrame, df2::AbstractDataFrame;
+             rtol::Real=atol>0 ? 0 : √eps, atol::Real=0,
+             nans::Bool=false, norm::Function=norm)
+
+Inexact equality comparison. `df1` and `df2` must have the same size and column names.
+Return  `true` if `isapprox` with given keyword arguments
+applied to all pairs of columns stored in `df1` and `df2` returns `true`.
+"""
+function Base.isapprox(df1::AbstractDataFrame, df2::AbstractDataFrame;
+                       atol::Real=0, rtol::Real=atol>0 ? 0 : √eps(),
+                       nans::Bool=false, norm::Function=norm)
+    size(df1) == size(df2) || throw(DimensionMismatch("dimensions must match: a has dims $(size(df1)), b has dims $(size(df2))"))
+    isequal(index(df1), index(df2)) || throw(ArgumentError("column names of passed data frames do not match"))
+    return all(isapprox.(eachcol(df1), eachcol(df2), atol=atol, rtol=rtol, nans=nans, norm=norm))
+end
 ##############################################################################
 ##
 ## Description
