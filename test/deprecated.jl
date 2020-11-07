6bc0ae8c68e223b6124bc92905e212cd4a497f05
Bogumił Kamiński <bkamins@sgh.waw.pl>
2020-11-07 22:20:24 +0100

    move All(args...) tests to deprecated.jl (#2515)

--- test/deprecated.jl
+++ test/deprecated.jl
@@ -231,6 +231,57 @@ end
           DataFrame(variable=:a, min=1, min2=1, max2=2, max=2)
 end
 
+@testset "All indexing" begin
+    df = DataFrame(a=1, b=2, c=3)
+
+    @test select(df, All(1,2)) == df[:, 1:2]
+    @test select(df, All(1,:b)) == df[:, 1:2]
+    @test select(df, All(:a,2)) == df[:, 1:2]
+    @test select(df, All(:a,:b)) == df[:, 1:2]
+    @test select(df, All(2,1)) == df[:, [2,1]]
+    @test select(df, All(:b,1)) == df[:, [2,1]]
+    @test select(df, All(2,:a)) == df[:, [2,1]]
+    @test select(df, All(:b,:a)) == df[:, [2,1]]
+
+    @test df[:, All(1,2)] == df[:, 1:2]
+    @test df[:, All(1,:b)] == df[:, 1:2]
+    @test df[:, All(:a,2)] == df[:, 1:2]
+    @test df[:, All(:a,:b)] == df[:, 1:2]
+    @test df[:, All(2,1)] == df[:, [2,1]]
+    @test df[:, All(:b,1)] == df[:, [2,1]]
+    @test df[:, All(2,:a)] == df[:, [2,1]]
+    @test df[:, All(:b,:a)] == df[:, [2,1]]
+
+    @test df[:, All(1,1,2)] == df[:, 1:2]
+    @test df[:, All(:a,1,:b)] == df[:, 1:2]
+    @test df[:, All(:a,2,:b)] == df[:, 1:2]
+    @test df[:, All(:a,:b,2)] == df[:, 1:2]
+    @test df[:, All(2,1,:a)] == df[:, [2,1]]
+
+    @test select(df, All(1,"b")) == df[:, 1:2]
+    @test select(df, All("a",2)) == df[:, 1:2]
+    @test select(df, All("a","b")) == df[:, 1:2]
+    @test select(df, All("b",1)) == df[:, [2,1]]
+    @test select(df, All(2,"a")) == df[:, [2,1]]
+    @test select(df, All("b","a")) == df[:, [2,1]]
+
+    @test df[:, All(1,"b")] == df[:, 1:2]
+    @test df[:, All("a",2)] == df[:, 1:2]
+    @test df[:, All("a","b")] == df[:, 1:2]
+    @test df[:, All("b",1)] == df[:, [2,1]]
+    @test df[:, All(2,"a")] == df[:, [2,1]]
+    @test df[:, All("b","a")] == df[:, [2,1]]
+
+    @test df[:, All("a",1,"b")] == df[:, 1:2]
+    @test df[:, All("a",2,"b")] == df[:, 1:2]
+    @test df[:, All("a","b",2)] == df[:, 1:2]
+    @test df[:, All(2,1,"a")] == df[:, [2,1]]
+
+    df = DataFrame(a1=1, a2=2, b1=3, b2=4)
+    @test df[:, All(r"a", Not(r"1"))] == df[:, [1,2,4]]
+    @test df[:, All(Not(r"1"), r"a")] == df[:, [2,4,1]]
+end
+
 @testset "deprecated DataFrame constructors" begin
     @test DataFrame(([1,2], [3,4])) == DataFrame([[1,2], [3,4]], :auto)
     @test DataFrame((categorical([1,2]), categorical([3,4]))) ==
