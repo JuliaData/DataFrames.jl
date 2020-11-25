9595755e6fcf6a6d12b3d39ec88a36f068654512
Milan Bouchet-Valat <nalimilan@club.fr>
2020-11-25 22:40:59 +0100

    Fix

--- test/utils.jl
+++ test/utils.jl
@@ -94,14 +94,13 @@ end
           :sum_skipmissing_div12
 end
 
-@test "nthreads and nthreads!" begin
+@testset "nthreads and nthreads!" begin
     @test DataFrames.nthreads() == 1
     @test DataFrames.nthreads!(2) == 2
     @test DataFrames.nthreads() == 2
     @test DataFrames.nthreads!(1) == 1 # reset to default
     @test_throws ArgumentError DataFrames.nthreads!(0)
     @test DataFrames.nthreads() == 1
-end
 end
 
 end # module
