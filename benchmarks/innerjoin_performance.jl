659ec7cd37314aaa465ecd6cca073b16ba473f1b
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-02-11 16:13:08 +0100

    improved benchmark design

--- benchmarks/innerjoin_performance.jl
+++ benchmarks/innerjoin_performance.jl
@@ -1,254 +1,97 @@
 using CategoricalArrays
-using CSV
 using DataFrames
-using Dates
 using PooledArrays
 using Random
 
-function run_innerjoin_tests(warmup::Bool = false)
-    warmup || run_innerjoin_tests(true)
-    Random.seed!(1234);
+fullgc() = (GC.gc(); GC.gc(); GC.gc(); GC.gc())
 
-    @info warmup ? "warmup" : "testing performance"
+@assert length(ARGS) == 6
+@assert ARGS[3] in ["int", "pool", "cat", "str"]
+@assert ARGS[4] in ["uniq", "dup", "manydup"]
+@assert ARGS[5] in ["sort", "rand"]
+@assert ARGS[6] in ["1", "2"]
 
-    df = DataFrame(llen=[], rlen=[], type=[], time=[], alloc=[], gc=[])
-    # change line below to match
-    # your preferred range of values tested
-    # and memory availability in your system
-    for llen in [10^3, 10^6], rlen in [2*10^7]
-        if warmup
-            llen, rlen = 1000, 1000
-        else
-            println("\nSize:")
-            @show llen, rlen
-        end
-        println()
+@info ARGS
 
-        warmup || @info "sorted string unique"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = sort!(string.(1:llen)), copycols=false)
-        df2 = DataFrame(id = sort!(string.(1:rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted string unique", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted string unique", x.time, x.bytes, x.gctime])
+llen = parse(Int, ARGS[1])
+rlen = parse(Int, ARGS[2])
+@assert llen > 1000
+@assert rlen > 2000
 
-        warmup || @info "shuffled string unique"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = shuffle!(string.(1:llen)), copycols=false)
-        df2 = DataFrame(id = shuffle!(string.(1:rlen)), copycols=false)
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled string unique", x.time, x.bytes, x.gctime])
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled string unique", x.time, x.bytes, x.gctime])
+pad = maximum(length.(string.((llen, rlen))))
 
-        warmup || @info "sorted string duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = sort!(rand(string.(1:llen), llen)), copycols=false)
-        df2 = DataFrame(id = sort!(rand(string.(1:rlen), rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted string duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted string duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "sorted string duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = sort!(rand(string.(1:llen ÷ 100), llen)), copycols=false)
-        df2 = DataFrame(id = sort!(rand(string.(1:rlen ÷ 100), rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted string duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted string duplicates many", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled string duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = rand(string.(1:llen), llen), copycols=false)
-        df2 = DataFrame(id = rand(string.(1:rlen), rlen), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled string duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled string duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled string duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = rand(string.(1:llen ÷ 100), llen), copycols=false)
-        df2 = DataFrame(id = rand(string.(1:rlen ÷ 100), rlen), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled string duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled string duplicates many", x.time, x.bytes, x.gctime])
-
-
-        warmup || @info "sorted int unique"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = sort!(1:llen), copycols=false)
-        df2 = DataFrame(id = sort!(1:rlen), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted int unique", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted int unique", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled int unique"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = shuffle(1:llen), copycols=false)
-        df2 = DataFrame(id = shuffle(1:rlen), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled int unique", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled int unique", x.time, x.bytes, x.gctime])
-
-        warmup || @info "sorted int duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = sort!(rand(1:llen, llen)), copycols=false)
-        df2 = DataFrame(id = sort!(rand(1:rlen, rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted int duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted int duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "sorted int duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = sort!(rand(1:llen ÷ 100, llen)), copycols=false)
-        df2 = DataFrame(id = sort!(rand(1:rlen ÷ 100, rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted int duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted int duplicates many", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled int duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = rand(1:llen, llen), copycols=false)
-        df2 = DataFrame(id = rand(1:rlen, rlen), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled int duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled int duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled int duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = rand(1:llen ÷ 100, llen), copycols=false)
-        df2 = DataFrame(id = rand(1:rlen ÷ 100, rlen), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled int duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled int duplicates many", x.time, x.bytes, x.gctime])
-
-
-        warmup || @info "sorted PooledArray duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = PooledArray(rand(string.(1:llen), llen)), copycols=false)
-        df2 = DataFrame(id = PooledArray(repeat(string.(1:rlen ÷ 10), inner=10)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted PooledArray duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted PooledArray duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "sorted PooledArray duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = PooledArray(rand(string.(1:llen ÷ 100), llen)), copycols=false)
-        df2 = DataFrame(id = PooledArray(repeat(string.(1:rlen ÷ 100), inner=10)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted PooledArray duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted PooledArray duplicates many", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled PooledArray duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = PooledArray(rand(string.(1:llen), llen)), copycols=false)
-        df2 = DataFrame(id = PooledArray(rand(string.(1:rlen ÷ 10), rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled PooledArray duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled PooledArray duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled PooledArray duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = PooledArray(rand(string.(1:llen ÷ 100), llen)), copycols=false)
-        df2 = DataFrame(id = PooledArray(rand(string.(1:rlen ÷ 100), rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled PooledArray duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled PooledArray duplicates many", x.time, x.bytes, x.gctime])
-
-
-        warmup || @info "sorted CategoricalArray duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = categorical(rand(string.(1:llen), llen)), copycols=false)
-        df2 = DataFrame(id = categorical(repeat(string.(1:rlen ÷ 10), inner=10)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted CategoricalArray duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted CategoricalArray duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "sorted CategoricalArray duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = categorical(rand(string.(1:llen ÷ 100), llen)), copycols=false)
-        df2 = DataFrame(id = categorical(repeat(string.(1:rlen ÷ 100), inner=10)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "sorted CategoricalArray duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "sorted CategoricalArray duplicates many", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled CategoricalArray duplicates"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = categorical(rand(string.(1:llen), llen)), copycols=false)
-        df2 = DataFrame(id = categorical(rand(string.(1:rlen ÷ 10), rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled CategoricalArray duplicates", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled CategoricalArray duplicates", x.time, x.bytes, x.gctime])
-
-        warmup || @info "shuffled CategoricalArray duplicates many"
-        df1, df2 = nothing, nothing
-        df1 = DataFrame(id = categorical(rand(string.(1:llen ÷ 100), llen)), copycols=false)
-        df2 = DataFrame(id = categorical(rand(string.(1:rlen ÷ 100), rlen)), copycols=false)
-        GC.gc()
-        x = @timed innerjoin(df1, df2, on=:id)
-        push!(df, [llen, rlen, "shuffled CategoricalArray duplicates many", x.time, x.bytes, x.gctime])
-        GC.gc()
-        x = @timed innerjoin(df2, df1, on=:id)
-        push!(df, [llen, rlen, "shuffled CategoricalArray duplicates many", x.time, x.bytes, x.gctime])
-
-        warmup && break
+if ARGS[3] == "int"
+    if ARGS[4] == "uniq"
+        col1 = [1:llen;]
+        col2 = [1:rlen;]
+    elseif ARGS[4] == "dup"
+        col1 = repeat(1:llen ÷ 2, inner=2)
+        col2 = repeat(1:rlen ÷ 2, inner=2)
+    else
+        @assert ARGS[4] == "manydup"
+        col1 = repeat(1:llen ÷ 20, inner=20)
+        col2 = repeat(1:rlen ÷ 20, inner=20)
     end
-    return df
+elseif ARGS[3] == "pool"
+    if ARGS[4] == "dup"
+        col1 = PooledArray(repeat(string.(1:llen ÷ 2, pad=pad), inner=2))
+        col2 = PooledArray(repeat(string.(1:rlen ÷ 2, pad=pad), inner=2))
+    else
+        @assert ARGS[4] == "manydup"
+        col1 = PooledArray(repeat(string.(1:llen ÷ 20, pad=pad), inner=20))
+        col2 = PooledArray(repeat(string.(1:rlen ÷ 20, pad=pad), inner=20))
+    end
+elseif ARGS[3] == "cat"
+    if ARGS[4] == "dup"
+        col1 = categorical(repeat(string.(1:llen ÷ 2, pad=pad), inner=2))
+        col2 = categorical(repeat(string.(1:rlen ÷ 2, pad=pad), inner=2))
+    else
+        @assert ARGS[4] == "manydup"
+        col1 = categorical(repeat(string.(1:llen ÷ 20, pad=pad), inner=20))
+        col2 = categorical(repeat(string.(1:rlen ÷ 20, pad=pad), inner=20))
+    end
+else
+    @assert ARGS[3] == "str"
+    if ARGS[4] == "uniq"
+        col1 = string.(1:llen, pad=pad)
+        col2 = string.(1:rlen, pad=pad)
+    elseif ARGS[4] == "dup"
+        col1 = repeat(string.(1:llen ÷ 2, pad=pad), inner=2)
+        col2 = repeat(string.(1:rlen ÷ 2, pad=pad), inner=2)
+    else
+        @assert ARGS[4] == "manydup"
+        col1 = repeat(string.(1:llen ÷ 20, pad=pad), inner=20)
+        col2 = repeat(string.(1:rlen ÷ 20, pad=pad), inner=20)
+    end
 end
 
-res = run_innerjoin_tests()
-CSV.write("results_$(now()).csv", res)
+Random.seed!(1234)
+
+if ARGS[5] == "rand"
+    shuffle!(col1)
+    shuffle!(col2)
+else
+    @assert ARGS[5] == "sort"
+end
+
+if ARGS[6] == "1"
+    df1 = DataFrame(id1 = col1);
+    df2 = DataFrame(id1 = col2);
+    innerjoin(df1[1:1000, :], df2[1:2000, :], on=:id1);
+    innerjoin(df2[1:2000, :], df1[1:1000, :], on=:id1);
+    fullgc();
+    @time innerjoin(df1, df2, on=:id1);
+    fullgc();
+    @time innerjoin(df2, df1, on=:id1);
+else
+    @assert ARGS[6] == "2"
+    df1 = DataFrame(id1 = col1, id2 = col1);
+    df2 = DataFrame(id1 = col1, id2 = col1);
+    innerjoin(df1[1:1000, :], df2[1:2000, :], on=[:id1, :id2]);
+    innerjoin(df2[1:2000, :], df1[1:1000, :], on=[:id1, :id2]);
+    fullgc();
+    @time innerjoin(df1, df2, on=[:id1, :id2]);
+    fullgc();
+    @time innerjoin(df2, df1, on=[:id1, :id2]);
+    df2 = DataFrame(id1 = col2, id2 = col2);
+end
