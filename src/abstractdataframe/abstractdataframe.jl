16e37c0fdd658946388b70b7e8f01f8276fd339e
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-03-11 20:42:31 +0100

    update docs of describe, unique, and nonunique

--- src/abstractdataframe/abstractdataframe.jl
+++ src/abstractdataframe/abstractdataframe.jl
@@ -503,8 +503,9 @@ where each row represents a variable and each column a summary statistic.
     - `:all` as the only `Symbol` argument to return all statistics.
     - A `function => name` pair where `name` is a `Symbol` or string. This will
       create a column of summary statistics with the provided name.
-- `cols` : a keyword argument allowing to select only a subset of columns from `df`
-  to describe. Can be any column selector ($COLUMNINDEX_STR; $MULTICOLUMNINDEX_STR).
+- `cols` : a keyword argument allowing to select only a subset or transformation
+  of columns from `df` to describe using `select(df, cols, copycols=false)`.
+  Can be any column selector accepted by `select`.
 
 # Details
 For `Real` columns, compute the mean, standard deviation, minimum, first
@@ -1187,8 +1188,9 @@ See also [`unique`](@ref) and [`unique!`](@ref).
 
 # Arguments
 - `df` : `AbstractDataFrame`
-- `cols` : a selector specifying the column(s) to compare. Can be any column
-  selector ($COLUMNINDEX_STR; $MULTICOLUMNINDEX_STR).
+- `cols` : a selector specifying the column(s) to compare using
+  `select(df, cols, copycols=false)` is unique.
+  `cols` can be any column selector accepted by `select`..
 
 # Examples
 ```julia
@@ -1281,9 +1283,9 @@ end
 
 Return a data frame containing only the first occurrence of unique rows in `df`.
 When `cols` is specified, the returned `DataFrame` contains
-complete rows, retaining in each case the first instance for which `df[cols]` is
-unique. `cols` can be any column selector ($COLUMNINDEX_STR;
-$MULTICOLUMNINDEX_STR).
+complete rows, retaining in each case the first instance for which
+`select(df, cols, copycols=false)` is unique.
+`cols` can be any column selector accepted by `select`.
 
 For `unique`, if `view=false` a freshly allocated `DataFrame` is returned,
 and if `view=true` then a `SubDataFrame` view into `df` is returned.
