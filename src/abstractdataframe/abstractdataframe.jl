1aa7022ba5a376af7fd16d0aaeb52a388ac5a78e
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-03-12 16:32:06 +0100

    Apply suggestions from code review
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/abstractdataframe/abstractdataframe.jl
+++ src/abstractdataframe/abstractdataframe.jl
@@ -1440,8 +1440,8 @@ Base.hcat(df1::AbstractDataFrame, df2::AbstractDataFrame, dfn::AbstractDataFrame
     vcat(dfs::AbstractDataFrame...;
          cols::Union{Symbol, AbstractVector{Symbol},
                      AbstractVector{<:AbstractString}}=:setequal,
-         indicator::Union{Nothing, SymbolOrString,
-                          Pair{<:SymbolOrString, <:AbstractVector}}=nothing)
+         indicator::Union{Nothing, Symbol, AbstractString,
+                          Pair{<:Union{Symbol, AbstractString}, <:AbstractVector}}=nothing)
 
 Vertically concatenate `AbstractDataFrame`s.
 
@@ -1613,7 +1613,7 @@ function Base.reduce(::typeof(vcat),
         if columnindex(res, col) > 0
             idx = findfirst(df -> columnindex(df, col) > 0, dfs)
             @assert idx !== nothing
-            throw(ArgumentError("indicator column name :$col exists in data frame " *
+            throw(ArgumentError("indicator column name :$col already exists in data frame " *
                                 " passed in position $idx"))
         end
 
