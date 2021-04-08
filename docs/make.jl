5f4f53345a3c8aea44927079ca1e38b9a10c9d83
RohitRathore1 <rohitrathore.imh55@gmail.com>
2021-04-08 10:28:46 +0530

    added metadata to avoid modules not defined error

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter, DataFrames
+DocMeta.setdocmeta!(DataFrames, :DocTestSetup, :(using DataFrames); recursive=true)
 
 # Workaround for JuliaLang/julia/pull/28625
 if Base.HOME_PROJECT[] !== nothing
