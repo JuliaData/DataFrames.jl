9a2c4d2be1a2ec7258bf914181ea569b55408f0f
Cloves Almeida <cjalmeida@gmail.com>
2021-04-13 06:50:55 -0300

    Improve structure of join benchmarks (#2707)

--- benchmarks/joins/join_performance.jl
+++ benchmarks/joins/join_performance.jl
@@ -5,97 +5,109 @@ using Random
 
 fullgc() = (GC.gc(true); GC.gc(true); GC.gc(true); GC.gc(true))
 
-@assert length(ARGS) == 7
-@assert ARGS[3] in ["int", "pool", "cat", "str"]
-@assert ARGS[4] in ["uniq", "dup", "manydup"]
-@assert ARGS[5] in ["sort", "rand"]
-@assert ARGS[6] in ["1", "2"]
-@assert ARGS[7] in ["inner", "left", "right", "outer", "semi", "anti"]
 
-@info ARGS
+function run_bench(llen, rlen, arr_type, dup_mode, sort_mode, num_keys, join_type)
 
-llen = parse(Int, ARGS[1])
-rlen = parse(Int, ARGS[2])
-@assert llen > 1000
-@assert rlen > 2000
 
-pad = maximum(length.(string.((llen, rlen))))
+    @assert llen > 1000
+    @assert rlen > 2000
 
-if ARGS[3] == "int"
-    if ARGS[4] == "uniq"
-        col1 = [1:llen;]
-        col2 = [1:rlen;]
-    elseif ARGS[4] == "dup"
-        col1 = repeat(1:llen ÷ 2, inner=2)
-        col2 = repeat(1:rlen ÷ 2, inner=2)
+    @assert arr_type in ["int", "pool", "cat", "str"]
+    @assert dup_mode in ["uniq", "dup", "manydup"]
+    @assert sort_mode in ["sort", "rand"]
+    @assert num_keys in ["1", "2"]
+    @assert join_type in ["inner", "left", "right", "outer", "semi", "anti"]
+
+    pad = maximum(length.(string.((llen, rlen))))
+
+    if arr_type == "int"
+        if dup_mode == "uniq"
+            col1 = [1:llen;]
+            col2 = [1:rlen;]
+        elseif dup_mode == "dup"
+            col1 = repeat(1:llen ÷ 2, inner=2)
+            col2 = repeat(1:rlen ÷ 2, inner=2)
+        else
+            @assert dup_mode == "manydup"
+            col1 = repeat(1:llen ÷ 20, inner=20)
+            col2 = repeat(1:rlen ÷ 20, inner=20)
+        end
+    elseif arr_type == "pool"
+        if dup_mode == "dup"
+            col1 = PooledArray(repeat(string.(1:llen ÷ 2, pad=pad), inner=2))
+            col2 = PooledArray(repeat(string.(1:rlen ÷ 2, pad=pad), inner=2))
+        else
+            @assert dup_mode == "manydup"
+            col1 = PooledArray(repeat(string.(1:llen ÷ 20, pad=pad), inner=20))
+            col2 = PooledArray(repeat(string.(1:rlen ÷ 20, pad=pad), inner=20))
+        end
+    elseif arr_type == "cat"
+        if dup_mode == "dup"
+            col1 = categorical(repeat(string.(1:llen ÷ 2, pad=pad), inner=2))
+            col2 = categorical(repeat(string.(1:rlen ÷ 2, pad=pad), inner=2))
+        else
+            @assert dup_mode == "manydup"
+            col1 = categorical(repeat(string.(1:llen ÷ 20, pad=pad), inner=20))
+            col2 = categorical(repeat(string.(1:rlen ÷ 20, pad=pad), inner=20))
+        end
     else
-        @assert ARGS[4] == "manydup"
-        col1 = repeat(1:llen ÷ 20, inner=20)
-        col2 = repeat(1:rlen ÷ 20, inner=20)
+        @assert arr_type == "str"
+        if dup_mode == "uniq"
+            col1 = string.(1:llen, pad=pad)
+            col2 = string.(1:rlen, pad=pad)
+        elseif dup_mode == "dup"
+            col1 = repeat(string.(1:llen ÷ 2, pad=pad), inner=2)
+            col2 = repeat(string.(1:rlen ÷ 2, pad=pad), inner=2)
+        else
+            @assert dup_mode == "manydup"
+            col1 = repeat(string.(1:llen ÷ 20, pad=pad), inner=20)
+            col2 = repeat(string.(1:rlen ÷ 20, pad=pad), inner=20)
+        end
     end
-elseif ARGS[3] == "pool"
-    if ARGS[4] == "dup"
-        col1 = PooledArray(repeat(string.(1:llen ÷ 2, pad=pad), inner=2))
-        col2 = PooledArray(repeat(string.(1:rlen ÷ 2, pad=pad), inner=2))
+
+    Random.seed!(1234)
+
+    if sort_mode == "rand"
+        shuffle!(col1)
+        shuffle!(col2)
     else
-        @assert ARGS[4] == "manydup"
-        col1 = PooledArray(repeat(string.(1:llen ÷ 20, pad=pad), inner=20))
-        col2 = PooledArray(repeat(string.(1:rlen ÷ 20, pad=pad), inner=20))
+        @assert sort_mode == "sort"
     end
-elseif ARGS[3] == "cat"
-    if ARGS[4] == "dup"
-        col1 = categorical(repeat(string.(1:llen ÷ 2, pad=pad), inner=2))
-        col2 = categorical(repeat(string.(1:rlen ÷ 2, pad=pad), inner=2))
+
+    joinfun = Dict("inner" => innerjoin, "left" => leftjoin,
+                    "right" => rightjoin, "outer" => outerjoin,
+                    "semi" => semijoin, "anti" => antijoin)[join_type]
+
+    if num_keys == "1"
+        df1 = DataFrame(id1=col1)
+        df2 = DataFrame(id1=col2)
+        joinfun(df1[1:1000, :], df2[1:2000, :], on=:id1)
+        joinfun(df2[1:2000, :], df1[1:1000, :], on=:id1)
+        fullgc()
+        t1 = @elapsed joinfun(df1, df2, on=:id1)
+        fullgc()
+        t2 = @elapsed joinfun(df2, df1, on=:id1)
     else
-        @assert ARGS[4] == "manydup"
-        col1 = categorical(repeat(string.(1:llen ÷ 20, pad=pad), inner=20))
-        col2 = categorical(repeat(string.(1:rlen ÷ 20, pad=pad), inner=20))
+        @assert num_keys == "2"
+        df1 = DataFrame(id1=col1, id2=col1)
+        df2 = DataFrame(id1=col2, id2=col2)
+        joinfun(df1[1:1000, :], df2[1:2000, :], on=[:id1, :id2])
+        joinfun(df2[1:2000, :], df1[1:1000, :], on=[:id1, :id2])
+        fullgc()
+        t1 = @elapsed joinfun(df1, df2, on=[:id1, :id2])
+        fullgc()
+        t2 = @elapsed joinfun(df2, df1, on=[:id1, :id2])
     end
-else
-    @assert ARGS[3] == "str"
-    if ARGS[4] == "uniq"
-        col1 = string.(1:llen, pad=pad)
-        col2 = string.(1:rlen, pad=pad)
-    elseif ARGS[4] == "dup"
-        col1 = repeat(string.(1:llen ÷ 2, pad=pad), inner=2)
-        col2 = repeat(string.(1:rlen ÷ 2, pad=pad), inner=2)
-    else
-        @assert ARGS[4] == "manydup"
-        col1 = repeat(string.(1:llen ÷ 20, pad=pad), inner=20)
-        col2 = repeat(string.(1:rlen ÷ 20, pad=pad), inner=20)
-    end
+
+    return (t1 + t2) / 2.0
 end
 
-Random.seed!(1234)
 
-if ARGS[5] == "rand"
-    shuffle!(col1)
-    shuffle!(col2)
-else
-    @assert ARGS[5] == "sort"
-end
+if abspath(PROGRAM_FILE) == @__FILE__
+    @info "$ARGS"
 
-const joinfun = Dict("inner" => innerjoin, "left" => leftjoin,
-                     "right" => rightjoin, "outer" => outerjoin,
-                     "semi" => semijoin, "anti" => antijoin)[ARGS[7]]
-
-if ARGS[6] == "1"
-    df1 = DataFrame(id1 = col1)
-    df2 = DataFrame(id1 = col2)
-    joinfun(df1[1:1000, :], df2[1:2000, :], on=:id1)
-    joinfun(df2[1:2000, :], df1[1:1000, :], on=:id1)
-    fullgc()
-    @time joinfun(df1, df2, on=:id1)
-    fullgc()
-    @time joinfun(df2, df1, on=:id1)
-else
-    @assert ARGS[6] == "2"
-    df1 = DataFrame(id1 = col1, id2 = col1)
-    df2 = DataFrame(id1 = col2, id2 = col2)
-    joinfun(df1[1:1000, :], df2[1:2000, :], on=[:id1, :id2])
-    joinfun(df2[1:2000, :], df1[1:1000, :], on=[:id1, :id2])
-    fullgc()
-    @time joinfun(df1, df2, on=[:id1, :id2])
-    fullgc()
-    @time joinfun(df2, df1, on=[:id1, :id2])
+    llen = parse(Int, ARGS[1])
+    rlen = parse(Int, ARGS[2])
+    t = run_bench(llen, rlen, ARGS[3], ARGS[4], ARGS[5], ARGS[6], ARGS[7])
+    @info("Timing: $t sec")
 end
--- benchmarks/joins/runtests.jl
+++ benchmarks/joins/runtests.jl
@@ -1,18 +1,102 @@
-@assert length(ARGS) == 3
-file_loc = joinpath(dirname(@__FILE__), "join_performance.jl")
-llen = ARGS[1]
-rlen = ARGS[2]
-type = ARGS[3]
+#!/usr/bin/env julia
+#
+# Run _join_ benchmarks
+#
+# Usage: julia runtests.jl [--single-process] <llen> <rlen> <join_type>
+#     --single-process      Runs all tests in a single julia process instead of
+#                           spawning multiple julia subprocesses
+#     <llen>                The length of the left dataframe
+#     <rlen>                The length of the right dataframe
+#     <join_type>           One of 'inner', 'left', 'right', 'outer', 'semi', 'anti'
+#
+# Note: Either provide all of <llen>, <rlen>, <join_type> arguments or leave blank
+#       to run all test cases (see `run_all_tests` below)
+
+
+using DataFrames
+include("join_performance.jl")
 
 # re-enable "cat" when
 # https://github.com/JuliaData/CategoricalArrays.jl/pull/327
 # and
 # https://github.com/JuliaData/CategoricalArrays.jl/pull/331
 # are merged and tagged
-for a3 in ["str", "int", "pool"], # "cat"],
-    a4 in ["uniq", "dup", "manydup"],
-    a5 in ["sort", "rand"],
-    a6 in ["1", "2"]
-    a4 == "uniq" && a3 in ["pool", "cat"] && continue
-    run(`julia $file_loc $llen $rlen $a3 $a4 $a5 $a6 $type`)
+
+const file_loc = joinpath(dirname(@__FILE__), "join_performance.jl")
+
+function run_all_tests(single_process)
+    cases = [
+        (100000, 50000000, "inner"),
+        (5000000, 10000000, "inner"),
+        (100000, 50000000, "left"),
+        (5000000, 10000000, "left"),
+        (100000, 50000000, "right"),
+        (5000000, 10000000, "right"),
+        (100000, 50000000, "outer"),
+        (5000000, 10000000, "outer"),
+        (100000, 50000000, "semi"),
+        (5000000, 10000000, "semi"),
+        (100000, 50000000, "anti"),
+        (5000000, 10000000, "anti")
+    ]
+
+    for (llen, rlen, join_type) in cases
+        run_test(single_process, llen, rlen, join_type)
+    end
+end
+
+function run_test(single_process, llen, rlen, join_type)
+    for
+        arr_type in ["str", "int", "pool"], # "cat"],
+        dup_mode in ["uniq", "dup", "manydup"],
+        sort_mode in ["sort", "rand"],
+        num_keys in ["1", "2"]
+        dup_mode == "uniq" && arr_type in ["pool", "cat"] && continue
+
+        if single_process
+            @info "Running test: $llen $rlen $arr_type $dup_mode $sort_mode $num_keys $join_type"
+            flush(stdout)
+
+            t = run_bench(llen, rlen, arr_type, dup_mode, sort_mode, num_keys, join_type)
+
+            @info "Timing: $t seconds"
+            flush(stdout)
+        else
+            run(`julia $file_loc $llen $rlen $arr_type $dup_mode $sort_mode $num_keys $join_type`)
+        end
+    end
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    path = pathof(DataFrames)
+    @info "DataFrames.jl at $path"
+
+    # do some argument processing
+    args = copy(ARGS)
+    single_process = false
+    pos = []
+    while length(args) > 0
+        arg = popfirst!(args)
+        if arg == "--single-process"
+            @info "Using single Julia process"
+            global single_process = true
+        elseif startswith(arg, "--")
+            @error("Unknown option $arg")
+            exit(1)
+        else
+            push!(pos, arg)
+        end
+    end
+
+    @assert length(args) in [0, 3] "Wrong positional arguments. Expect [llen, rlen, join_type], got $args."
+
+    if length(pos) == 0
+        @info "Running all tests"
+        run_all_tests(single_process)
+    else
+        llen, rlen, join_type = pos
+        llen = parse(Int, llen)
+        rlen = parse(Int, rlen)
+        run_test(single_process, llen, rlen, join_type)
+    end
 end
