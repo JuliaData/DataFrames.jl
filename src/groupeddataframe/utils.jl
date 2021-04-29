58c0e57dfb5f19f48969aa283feadfeba9d95264
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-04-29 10:51:31 +0200

    revert @spawn_for_chunks

--- src/groupeddataframe/utils.jl
+++ src/groupeddataframe/utils.jl
@@ -1,3 +1,77 @@
+# Compute chunks of indices, each with at least `basesize` entries
+# This method ensures balanced sizes by avoiding a small last chunk
+function split_indices(len::Integer, basesize::Integer)
+    len′ = Int64(len) # Avoid overflow on 32-bit machines
+    np = max(1, div(len, basesize))
+    return (Int(1 + ((i - 1) * len′) ÷ np):Int((i * len′) ÷ np) for i in 1:np)
+end
+
+if VERSION >= v"1.4"
+    function _spawn_for_chunks_helper(iter, lbody, basesize)
+        lidx = iter.args[1]
+        range = iter.args[2]
+        quote
+            let x = $(esc(range)), basesize = $(esc(basesize))
+                @assert firstindex(x) == 1
+
+                nt = Threads.nthreads()
+                len = length(x)
+                if nt > 1 && len > basesize
+                    tasks = [Threads.@spawn begin
+                                 for i in p
+                                     local $(esc(lidx)) = @inbounds x[i]
+                                     $(esc(lbody))
+                                 end
+                             end
+                             for p in split_indices(len, basesize)]
+                    foreach(wait, tasks)
+                else
+                    for i in eachindex(x)
+                        local $(esc(lidx)) = @inbounds x[i]
+                        $(esc(lbody))
+                    end
+                end
+            end
+            nothing
+        end
+    end
+else
+    function _spawn_for_chunks_helper(iter, lbody, basesize)
+        lidx = iter.args[1]
+        range = iter.args[2]
+        quote
+            let x = $(esc(range))
+                for i in eachindex(x)
+                    local $(esc(lidx)) = @inbounds x[i]
+                    $(esc(lbody))
+                end
+            end
+            nothing
+        end
+    end
+end
+
+"""
+    @spawn_for_chunks basesize for i in range ... end
+
+Parallelize a `for` loop by spawning separate tasks
+iterating each over a chunk of at least `basesize` elements
+in `range`.
+
+A number of task higher than `Threads.nthreads()` may be spawned,
+since that can allow for a more efficient load balancing in case
+some threads are busy (nested parallelism).
+"""
+macro spawn_for_chunks(basesize, ex)
+    if !(isa(ex, Expr) && ex.head === :for)
+        throw(ArgumentError("@spawn_for_chunks requires a `for` loop expression"))
+    end
+    if !(ex.args[1] isa Expr && ex.args[1].head === :(=))
+        throw(ArgumentError("nested outer loops are not currently supported by @spawn_for_chunks"))
+    end
+    return _spawn_for_chunks_helper(ex.args[1], ex.args[2], basesize)
+end
+
 # "kernel" functions for hashrows()
 # adjust row hashes by the hashes of column elements
 function hashrows_col!(h::Vector{UInt},
@@ -5,7 +79,7 @@ function hashrows_col!(h::Vector{UInt},
                        v::AbstractVector{T},
                        rp::Nothing,
                        firstcol::Bool) where T
-    Threads.@threads for i in eachindex(h)
+    @spawn_for_chunks 1_000_000 for i in eachindex(h)
         @inbounds begin
             el = v[i]
             h[i] = hash(el, h[i])
@@ -33,18 +107,18 @@ function hashrows_col!(h::Vector{UInt},
         fira = firstindex(ra)
 
         hashes = Vector{UInt}(undef, length(rp))
-        Threads.@threads for i in eachindex(hashes)
+        @spawn_for_chunks 1_000_000 for i in eachindex(hashes)
             @inbounds hashes[i] = hash(rp[i+firp-1])
         end
 
         # here we rely on the fact that `DataAPI.refpool` has a continuous
         # block of indices
-        Threads.@threads for i in eachindex(h)
+        @spawn_for_chunks 1_000_000 for i in eachindex(h)
             @inbounds ref = ra[i+fira-1]
             @inbounds h[i] = hashes[ref+1-firp]
         end
     else
-        Threads.@threads for i in eachindex(h, v)
+        @spawn_for_chunks 1_000_000 for i in eachindex(h, v)
             @inbounds h[i] = hash(v[i], h[i])
         end
     end
