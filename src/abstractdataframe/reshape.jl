ca1dc679060262286536442618f138ac67d969b4
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-08-24 17:11:50 +0200

    Update src/abstractdataframe/reshape.jl
    Co-authored-by: Milan Bouchet-Valat <nalimilan@club.fr>

--- src/abstractdataframe/reshape.jl
+++ src/abstractdataframe/reshape.jl
@@ -418,7 +418,7 @@ function _unstack(df::AbstractDataFrame, rowkeys::AbstractVector{Int},
         mask_filled[row_id, col_id] = true
     end
 
-    # note that Symbol(renamecols(colref_map)) must produce unique column names
+    # note that Symbol(renamecols(x)) must produce unique column names
     # and names between df1 and df2 must be unique
     df1 = df[row_group_row_idxs, g_rowkey.cols]
     df2 = DataFrame(unstacked_val, Symbol[Symbol(renamecols(x)) for x in colref_map],
