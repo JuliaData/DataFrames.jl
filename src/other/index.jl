8337dddc098eadb9d18ba71595e87cd6962fd729
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-09-15 16:49:47 +0200

    Revert "remove special path for :"
    This reverts commit bc29631e9f676a8d02dc7998ad163bec8b053b47.

--- src/other/index.jl
+++ src/other/index.jl
@@ -221,6 +221,7 @@ end
     isempty(idx.cols) ? (1:length(x)) : throw(ArgumentError("All(args...) is not supported: use Cols(args...) instead"))
 @inline Base.getindex(x::AbstractIndex, idx::Cols) =
     isempty(idx.cols) ? Int[] : union(getindex.(Ref(x), idx.cols)...)
+@inline Base.getindex(x::AbstractIndex, idx::Cols{Tuple{typeof(:)}}) = x[:]
 @inline Base.getindex(x::AbstractIndex, idx::Cols{<:Tuple{Function}}) =
     findall(idx.cols[1], names(x))
 
