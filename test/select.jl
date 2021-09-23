7fcb93baf95b985f1ca4a691eb066c7f5f72b3f4
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-09-23 21:15:51 +0200

    avoid not specialized Pair issue (#2889)

--- test/select.jl
+++ test/select.jl
@@ -1678,4 +1678,18 @@ end
     @test select(df, :a => ByRow(f) => :a) == DataFrame(a=1:3)
 end
 
+@testset "vectors of pairs with non-specific type are accepted" begin
+    df = DataFrame(x=[1,2,3])
+    @test combine(df, [1 => length => :a, 1 => length => "b"]) == DataFrame(a=3, b=3)
+    @test combine(df, [:x => length => :a, 1 => :b]) == DataFrame(a=3, b=1:3)
+    gdf = groupby(df, :x)
+    @test combine(gdf, [1 => length => :a, 1 => length => "b"]) == DataFrame(x=1:3, a=1, b=1)
+    @test combine(gdf, [:x => length => :a, 1 => :b]) == DataFrame(x=1:3, a=1, b=1:3)
+    sdf = view(df, :, :)
+    @test select(sdf, [1 => length => :a, 1 => length => "b"]) == DataFrame(a=[3, 3, 3], b=[3, 3, 3])
+    @test select(sdf, [:x => length => :a, 1 => :b]) == DataFrame(a=3, b=1:3)
+    @test_throws ArgumentError select(sdf, [1 => length => :a, 1 => length => "b"], copycols=false)
+    @test_throws ArgumentError select(sdf, [:x => length => :a, 1 => :b], copycols=false)
+end
+
 end # module
