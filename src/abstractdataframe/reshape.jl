ae8c7634a989311f40e3d82a210494458dd7194f
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-04-30 09:42:39 +0200

    minor performance improvement

--- src/abstractdataframe/reshape.jl
+++ src/abstractdataframe/reshape.jl
@@ -164,8 +164,8 @@ function stack(df::AbstractDataFrame,
         # (note that copyto! inserts levels in their order of appearance)
         nms = names(df, ints_measure_vars)
         simnms = similar(nms, variable_eltype)
-        copyto!(simnms, nms)
         catnms = simnms isa Vector ? PooledArray(simnms) : simnms
+        copyto!(catnms, nms)
     end
     return DataFrame(AbstractVector[[repeat(df[!, c], outer=N) for c in ints_id_vars]..., # id_var columns
                                     repeat(catnms, inner=nrow(df)),                       # variable
