a5becd2124ebf5347f24d6ef4799c3c92eb6c77e
krynju <gulinski.krystian@gmail.com>
2022-11-08 20:49:43 +0100

    import cleanup/style to make it at least readable

--- src/new_module/SeparateModule.jl
+++ src/new_module/SeparateModule.jl
@@ -1,13 +1,13 @@
 module SeparateModule
 
-using ..SeparateModuleIndex: AbstractIndex,ColumnIndex,MultiColumnIndex, make_unique, AsTable,_names,funname
 import ..SeparateModuleIndex: funname
 
-
-using Printf: @sprintf
-using InvertedIndices: Not, InvertedIndices
-using DataAPI: Between, All, Cols, nrow, DataAPI
 using ..DataFrames: AbstractDataFrame, DataFrame, DataFrameRow, SubDataFrame
+using ..SeparateModuleIndex:
+    AbstractIndex, AsTable, ColumnIndex, MultiColumnIndex, _names, funname, make_unique
+using DataAPI: All, Between, Cols, DataAPI, nrow
+using InvertedIndices: InvertedIndices, Not
+using Printf: @sprintf
 using Tables: ByRow
 
 
@@ -16,5 +16,5 @@ include("selection.jl")
 
 
 
-export normalize_selection , make_pair_concrete,  broadcast_pair,_manipulate
+export normalize_selection, make_pair_concrete, broadcast_pair, _manipulate
 end
--- src/new_module_index/SeparateModuleIndex.jl
+++ src/new_module_index/SeparateModuleIndex.jl
@@ -3,9 +3,9 @@ module SeparateModuleIndex
 
 
 
+using DataAPI: All, Between, Cols, DataAPI, nrow
+using InvertedIndices: InvertedIndices, Not
 using Printf: @sprintf
-using InvertedIndices: Not, InvertedIndices
-using DataAPI: Between, All, Cols, nrow, DataAPI
 # using ..DataFrames: AbstractDataFrame, DataFrame, DataFrameRow, SubDataFrame
 # using Tables: ByRow
 
@@ -14,5 +14,9 @@ include("index.jl")
 
 
 
-export  _findall,add_names,split_to_chunks,funname,SubIndex,_names,@spawn_or_run,@spawn_or_run_task,@spawn_for_chunks, AsTable, ColumnIndex, MultiColumnIndex, Index, COLUMNINDEX_STR,MULTICOLUMNINDEX_STR,SymbolOrString,AbstractIndex,MULTICOLUMNINDEX_TUPLE,make_unique
+export
+    _findall, add_names, split_to_chunks, funname, SubIndex, _names, @spawn_or_run,
+    @spawn_or_run_task, @spawn_for_chunks, AsTable, ColumnIndex, MultiColumnIndex, Index,
+    COLUMNINDEX_STR, MULTICOLUMNINDEX_STR, SymbolOrString, AbstractIndex,
+    MULTICOLUMNINDEX_TUPLE, make_unique
 end
