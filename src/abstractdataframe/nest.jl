792d35513f43ac8eaec8060f396fdb5562638928
Bogumił Kamiński <bkamins@sgh.waw.pl>
2023-01-05 10:21:47 +0100

    initial implementation

--- src/abstractdataframe/nest.jl
+++ src/abstractdataframe/nest.jl
@@ -1,7 +1,7 @@
 """
-    nest(gdf::GroupedDataFrame, cols::Pair{<:AbstractString}...)
-    nest(gdf::GroupedDataFrame, cols::Pair{Symbol}...)
-    nest(gdf::GroupedDataFrame)
+    nest(gdf::GroupedDataFrame, cols::Pair{<:AbstractString}...; view::Bool=false)
+    nest(gdf::GroupedDataFrame, cols::Pair{Symbol}...; view::Bool=false)
+    nest(gdf::GroupedDataFrame; view::Bool=false)
 
 Return a data frame with one row for each group in `gdf` where
 or or more columns contain a data frame of the rows that belong to that group.
@@ -14,6 +14,10 @@ The returned data frame has all grouping columns of `gdf`, followed by one
 or more columns where `column_name` is the name of the column storing data frames,
 and every data frame consists of columns picked by `column_selector` values
 computed for each group of `gdf`.
+
+If `view=false` (the default) the nested data frames will hold copies of
+data from the source data frame. If `view=true` views of the source data frame
+will be created.
 
 TODO: metadata
 
@@ -143,29 +147,28 @@ julia> n3.yz
 ─────┼─────────────
    1 │    14  d
 """
-nest(gdf::GroupedDataFrame, cols::Pair{<:Any, <:AbstractString}...) =
-    combine(gdf, (sdf -> (; Symbol(dst) => select(sdf, index(sdf)[src]))
+nest(gdf::GroupedDataFrame, cols::Pair{<:Any, <:AbstractString}...;
+     view::Bool=false) =
+    combine(gdf, (sdf -> (; Symbol(dst) => select(sdf, index(sdf)[src], copycols=!view))
                   for (src, dst) in cols)...)
-nest(gdf::GroupedDataFrame, cols::Pair{<:Any, Symbol}...) =
-    combine(gdf, (sdf -> (; dst => select(sdf, index(sdf)[src]))
+nest(gdf::GroupedDataFrame, cols::Pair{<:Any, Symbol}...; view::Bool=false) =
+    combine(gdf, (sdf -> (; dst => select(sdf, index(sdf)[src], copycols=!view))
                   for (src, dst) in cols)...)
-nest(gdf::GroupedDataFrame) = nest(gdf, valuecols(gdf) => :data)
+nest(gdf::GroupedDataFrame; view::Bool=false) =
+    nest(gdf, valuecols(gdf) => :data, view=view)
 
 """
     unnest(df::AbstractDataFrame, src::ColumnIndex...;
            cols::Union{Symbol, AbstractVector{Symbol},
                        AbstractVector{<:AbstractString}}=:setequal,
-           promote::Bool=true,
-           makeunique::Bool=false, flatten::Bool=true)
+           promote::Bool=true, makeunique::Bool=false)
 
-Extract the contents of one or more columns `cols` in `df` that contain data frames,
-returning a data frame with as many rows and columns as the nested data frames contain,
-in addition to original columns.
-The newly created columns are stored at the end of the data frame (and the `src` columns are
-dropped).
-
-Each `src` column must contain a `NamedTuple`, a `DataFrameRow`, a
-`Tables.AbstractRow`, or Tables.jl table.
+Extract the contents of one or more columns `cols` in `df` that contain
+Tables.jl tables, returning a data frame with as many rows and columns as the
+nested data frames contain, in addition to original columns, whose contents
+gets appropriately repeated to match the number of rows of the unnested tables.
+The newly created columns are stored at the end of the data frame (and the
+`src` columns are dropped).
 
 `cols` (default `:setequal`) and `promote` (default `true`) keyword arguments
 have the same meaning as in [`push!`](@ref).
@@ -174,9 +177,6 @@ If `makeunique=false` (the default) produced column names must be unique.
 If `makeunique=true` then duplicate column names will be suffixed with `_i`
 (`i` starting at `1` for the first duplicate).
 
-If `flatten=true` (the default) then newly created columns are flattened
-using [`flatten`](@ref) with `scalar=Missing` keyword argument.
-
 TODO: metadata
 
 """
@@ -184,7 +184,7 @@ function unnest(df::AbstractDataFrame, src::ColumnIndex...;
                 cols::Union{Symbol, AbstractVector{Symbol},
                             AbstractVector{<:AbstractString}}=:setequal,
                 promote::Bool=(cols in [:union, :subset]),
-                makeunique::Bool=false, flatten::Bool=true)
+                makeunique::Bool=false)
     ref_df = select(df, Not(collect(Any, src)))
     col_count = ncol(ref_df)
     for idx in src
@@ -192,22 +192,96 @@ function unnest(df::AbstractDataFrame, src::ColumnIndex...;
         tmp_df = DataFrame()
         for v in col
             if v isa DataFrame # produce DataFrameRow
-                # if flatten=false make a copy to avoid aliases
-                v = DataFrame([n => [flatten ? c : copy(c)]
-                               for (n, c) in pairs(eachcol(v))],
+                v = DataFrame([n => [c] for (n, c) in pairs(eachcol(v))],
                               copycols=false) |> only
-            elseif Tables.istable(v) # produce NamedTuple
+            else # produce NamedTuple
                 v = Tables.columntable(v)
             end
             push!(tmp_df, v, cols=cols, promote=promote)
         end
         hcat!(ref_df, tmp_df, makeunique=makeunique, copycols=false)
     end
-    return if flatten
-        DataFrames.flatten(ref_df, col_count+1:ncol(ref_df), scalar=Missing)
-    else
-        ref_df
+    return flatten(ref_df, col_count+1:ncol(ref_df), scalar=Missing)
+end
+
+"""
+    expand(df::AbstractDataFrame, src::ColumnIndex...;
+           cols::Union{Symbol, AbstractVector{Symbol},
+                       AbstractVector{<:AbstractString}}=:setequal,
+           promote::Bool=true, makeunique::Bool=false)
+
+Extract the contents of one or more columns `cols` in `df` that contain
+`NamedTuple`, a `DataFrameRow`, a `Tables.AbstractRow` elements
+returning a data frame with expanded columns, in addition to original columns.
+The newly created columns are stored at the end of the data frame (and the
+`src` columns are dropped).
+
+`cols` (default `:setequal`) and `promote` (default `true`) keyword arguments
+have the same meaning as in [`push!`](@ref).
+
+If `makeunique=false` (the default) produced column names must be unique.
+If `makeunique=true` then duplicate column names will be suffixed with `_i`
+(`i` starting at `1` for the first duplicate).
+
+TODO: metadata
+
+"""
+function expand(df::AbstractDataFrame, src::ColumnIndex...;
+                cols::Union{Symbol, AbstractVector{Symbol},
+                            AbstractVector{<:AbstractString}}=:setequal,
+                promote::Bool=(cols in [:union, :subset]),
+                makeunique::Bool=false)
+    ref_df = select(df, Not(collect(Any, src)))
+    for idx in src
+        col = df[!, idx]
+        tmp_df = DataFrame()
+        for v in col
+            push!(tmp_df, v, cols=cols, promote=promote)
+        end
+        hcat!(ref_df, tmp_df, makeunique=makeunique, copycols=false)
     end
+    return ref_df
+end
+
+"""
+    expand!(df::AbstractDataFrame, src::ColumnIndex...;
+            cols::Union{Symbol, AbstractVector{Symbol},
+                        AbstractVector{<:AbstractString}}=:setequal,
+            promote::Bool=true, makeunique::Bool=false)
+
+Extract in-place the contents of one or more columns `cols` in `df` that contain
+`NamedTuple`, a `DataFrameRow`, a `Tables.AbstractRow` elements
+returning a data frame with expanded columns, in addition to original columns.
+The newly created columns are stored at the end of the data frame (and the
+`src` columns are dropped).
+
+`cols` (default `:setequal`) and `promote` (default `true`) keyword arguments
+have the same meaning as in [`push!`](@ref).
+
+If `makeunique=false` (the default) produced column names must be unique.
+If `makeunique=true` then duplicate column names will be suffixed with `_i`
+(`i` starting at `1` for the first duplicate).
+
+TODO: metadata
+
+"""
+function expand!(df::AbstractDataFrame, src::ColumnIndex...;
+                 cols::Union{Symbol, AbstractVector{Symbol},
+                             AbstractVector{<:AbstractString}}=:setequal,
+                 promote::Bool=(cols in [:union, :subset]),
+                 makeunique::Bool=false)
+    tmp_dfs = DataFrame[]
+    for idx in src
+        col = df[!, idx]
+        tmp_df = DataFrame()
+        for v in col
+            push!(tmp_df, v, cols=cols, promote=promote)
+        end
+        push!(tmp_dfs, tmp_df)
+    end
+    ref_df = select!(df, Not(collect(Any, src)))
+    hcat!(ref_df, tmp_dfs..., makeunique=makeunique, copycols=false)
+    return ref_df
 end
 
 """
@@ -328,7 +402,7 @@ function flatten(df::AbstractDataFrame,
     end
 
     col1 = first(idxcols)
-    lengths = Int[length_maybe_scalar(x, scalar) for x in df[!, col1]]
+    lengths = Int[x isa scalar ? -1 : length(x) for x in df[!, col1]]
     for (i, coli) in enumerate(idxcols)
         i == 1 && continue
         update_lengths!(lengths, df[!, coli], scalar, df, col1, coli)
@@ -364,13 +438,11 @@ function flatten(df::AbstractDataFrame,
     return new_df
 end
 
-length_maybe_scalar(v, scalar::Type) = v isa scalar ? -1 : length(v)
-
 function update_lengths!(lengths::Vector{Int}, col::AbstractVector, scalar::Type,
                          df::AbstractDataFrame, col1, coli)
     for (i, v) in enumerate(col)
-        lv = length_maybe_scalar(v, scalar)
-        lv == -1 && continue
+        v isa scalar && continue
+        lv = length(v)
         if lengths[i] == -1
             lengths[i] = lv
         elseif lengths[i] != lv
