e27e8cd515da28624af2eec34582b186ab048d50
Bogumił Kamiński <bkamins@sgh.waw.pl>
2023-06-15 15:53:58 +0200

    Update test/sort.jl

--- test/sort.jl
+++ test/sort.jl
@@ -404,18 +404,21 @@ end
 end
 
 @testset "issue #3340, mostly downstream testing for SortingAlgorithms.jl and Base.Sort" begin
-    df = DataFrame(units = round.(Int, 1 ./ rand(2000)),
-                   sku = [rand() < .084 ? missing : String31(randstring(8)) for _ in 1:2000])
-    @test issorted(sort(select(df, [:sku, :units]), :sku).sku)
-    @test issorted(sort(select(df, [:sku, :units]), :units).units)
-    @test issorted(sort(select(df, [:sku, :units]).sku))
-    @test issorted(sort(df, :sku).sku)
+    Random.seed!(1234)
+    for i in 1:10
+        df = DataFrame(units = round.(Int, 1 ./ rand(2000)),
+                       sku = [rand() < .084 ? missing : String31(randstring(8)) for _ in 1:2000])
+        @test issorted(sort(select(df, [:sku, :units]), :sku).sku)
+        @test issorted(sort(select(df, [:sku, :units]), :units).units)
+        @test issorted(sort(select(df, [:sku, :units]).sku))
+        @test issorted(sort(df, :sku).sku)
 
-    o = Base.Order.Perm(Base.Order.ForwardOrdering(), df.sku)
-    a = DataFrames.SortingAlgorithms.TimSortAlg()
-    x = collect(1:nrow(df))
-    sort!(x, a, o)
-    @test issorted(df[x, :sku])
+        o = Base.Order.Perm(Base.Order.ForwardOrdering(), df.sku)
+        a = DataFrames.SortingAlgorithms.TimSortAlg()
+        x = collect(1:nrow(df))
+        sort!(x, a, o)
+        @test issorted(df[x, :sku])
+    end
 end
 
 end # module
