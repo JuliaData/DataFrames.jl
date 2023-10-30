f43864cd96d6cc6caa13edf52f1934d3ed978b8a
Lee Iverson <leei@sociologi.ca>
2023-10-30 16:43:19 -0700

    Add `outerjoin!` method for two dataframes.

--- src/join/inplace.jl
+++ src/join/inplace.jl
@@ -205,3 +205,106 @@ function compose_joined_rcol!(rcol::AbstractVector,
     return rcol_joined
 end
 
+"""
+    outerjoin!(df1, df2; on, makeunique=false, mergeduplicates=nothing, source=nothing, 
+               matchmissing=:error)
+
+Perform an outer join of two data frame objects by updating into `df1`.
+An outer join includes rows with keys that appear in either
+of the passed data frames.
+
+The order of rows in the result is undefined and may change in future releases.
+
+In the returned data frame the type of the columns on which the data frames are
+joined is determined by the element type of these columns both `df1` and `df2`.
+This behavior may change in future releases.
+
+# Arguments
+- `df1`, `df2` : the `AbstractDataFrames` to be joined
+
+# Keyword Arguments
+- `on` : The names of the key columns on which to join the data frames.
+  This can be a single name, or a vector of names (for joining on multiple
+  columns). When joining only two data frames, a `left=>right` pair of names
+  can be used instead of a name, for the case where a key has different names
+  in `df1` and `df2` (it is allowed to mix names and name pairs in a vector).
+  Key values are compared using `isequal`. `on` is a required argument.
+- `makeunique` : if `false` (the default), an error will be raised
+  if duplicate names are found in columns not joined on;
+  if `true`, duplicate names will be suffixed with `_i`
+  (`i` starting at 1 for the first duplicate). (deprecated)
+- `mergeduplicates` : defines what to do if `name` already exists in `df` and `makeunique`
+  is false. It should be given a Function that combines the values of all of the duplicated
+  columns which will be passed as a varargs. The return value is used.
+- `source` : Default: `nothing`. If a `Symbol` or string, adds indicator
+  column with the given name for whether a row appeared in only `df1` (`"left_only"`),
+  only `df2` (`"right_only"`) or in both (`"both"`). If the name is already in use,
+  the column name will be modified if `makeunique=true`.
+  This argument is only supported when joining exactly two data frames.
+- `matchmissing` : if equal to `:error` throw an error if `missing` is present
+  in `on` columns; if equal to `:equal` then `missing` is allowed and missings are
+  matched.
+
+All columns of the returned data frame will support missing values.
+
+It is not allowed to join on columns that contain `NaN` or `-0.0` in real or
+imaginary part of the number. If you need to perform a join on such values use
+CategoricalArrays.jl and transform a column containing such values into a
+`CategoricalVector`.
+
+When merging `on` categorical columns that differ in the ordering of their
+levels, the ordering of the left data frame takes precedence over the ordering
+of the right data frame.
+
+Metadata: table-level `:note`-style metadata and column-level `:note`-style metadata
+for key columns is preserved only for keys which are defined in all passed tables
+and have the same value.
+Column-level `:note`-style metadata is preserved for all other columns.
+
+See also: [`innerjoin`](@ref), [`leftjoin`](@ref), [`rightjoin`](@ref),
+          [`semijoin`](@ref), [`antijoin`](@ref), [`crossjoin`](@ref).
+
+# Examples
+```jldoctest
+julia> name = DataFrame(ID=[1, 2, 3], Name=["John Doe", "Jane Doe", "Joe Blogs"])
+3×2 DataFrame
+ Row │ ID     Name
+     │ Int64  String
+─────┼──────────────────
+   1 │     1  John Doe
+   2 │     2  Jane Doe
+   3 │     3  Joe Blogs
+
+julia> job = DataFrame(ID=[1, 2, 4], Job=["Lawyer", "Doctor", "Farmer"])
+3×2 DataFrame
+ Row │ ID     Job
+     │ Int64  String
+─────┼───────────────
+   1 │     1  Lawyer
+   2 │     2  Doctor
+   3 │     4  Farmer
+
+julia> outerjoin!(name, job, on = :ID)
+4×3 DataFrame
+ Row │ ID     Name       Job
+     │ Int64  String?    String?
+─────┼───────────────────────────
+   1 │     1  John Doe   Lawyer
+   2 │     2  Jane Doe   Doctor
+   3 │     3  Joe Blogs  missing
+   4 │     4  missing    Farmer
+```
+"""
+function outerjoin!(df1::AbstractDataFrame, df2::AbstractDataFrame;
+    on::Union{<:OnType, AbstractVector}=Symbol[], 
+    makeunique::Bool=false, mergeduplicates::MergeDuplicates=nothing,
+    source::Union{Nothing, Symbol, AbstractString}=nothing,
+    matchmissing::Symbol=:error)
+
+    leftjoin!(df1, df2, on=on, makeunique=makeunique, mergeduplicates=mergeduplicates, source=source, matchmissing=matchmissing)
+
+    aj = antijoin(df2, df1, on=on, makeunique=makeunique, mergeduplicates=mergeduplicates, matchmissing=matchmissing)
+    append!(df1, aj)
+
+    return df1
+end
