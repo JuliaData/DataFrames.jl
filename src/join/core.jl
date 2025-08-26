cd3169ca9df660e50d2eb96990d634bfc2ebb893
Bogumil Kaminski <bkamins@sgh.waw.pl>
2025-08-26 21:44:21 +0200

    fix tuple-like hashing

--- src/join/core.jl
+++ src/join/core.jl
@@ -40,11 +40,8 @@ Base.hash(ocr1::OnColRow, h::UInt) = throw(MethodError(hash, (ocr1, h)))
 function _prehash(oc::OnCol)
     h = oc.h
     resize!(h, oc.len)
-    h0 = @static if Base.VERSION >= v"1.13.0-DEV"
-             Base.HASH_SEED
-         else
-             Base.tuplehash_seed
-         end
+    h0 = @static Base.VERSION >= v"1.13.0-DEV" ? Base.HASH_SEED : UInt(0)
+    h0 = h0 ⊻ Base.tuplehash_seed
     fill!(h, h0)
     for col in reverse(oc.cols)
         h .= hash.(col, h)
